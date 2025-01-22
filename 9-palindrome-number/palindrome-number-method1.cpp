a462f3f28f07c12989ccfa9ec9ea20155d8da42c
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-01-22 16:53:51 +0530

    Update and rename palindrome-number-method1 to palindrome-number-method1.cpp

---
+++ 9-palindrome-number/palindrome-number-method1.cpp
@@ -0,0 +1,22 @@
+// Reversing entire number => 
+// (Method 1)
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false; // eg. as given in example 2 in leetcode qn 9
+        int original = x, digit = 0;
+        long long rev = 0; // using long long as reverse number can exceed the int range for larger int values(x) as
+                           // constraints for x are [-2^31 <= x <= 2^31 - 1]
+        while (x != 0) {
+            digit = x % 10; // extracting the digits
+            x = x / 10; // making the number smaller
+            rev = rev * 10 + digit;
+        }
+        if (original == rev) return true;
+        return false;
+    }
+};
+
+// T.C => O(log(n)) -> {log to the base 10}
+// S.C => O(1)
