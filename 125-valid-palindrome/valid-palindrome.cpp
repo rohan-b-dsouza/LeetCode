6d7228b07e7a1c59fee4e45f4a54bb1b6a5c871f
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-01-23 21:40:34 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (88.38%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,18 +1,22 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string str = "";
-        for (int i = 0; i < s.length(); i++) {
-            if (isalnum(s[i])) str += tolower(s[i]);
-        }
-        int n = str.length();
         int i = 0;
-        while (i != n / 2) {
-            if (str[i] != str[n - i - 1]) return false;
-            i++;
+        int j = s.length() - 1;
+        while (i < j) {
+            if (!isalnum(s[i])) {
+                i++;
+                continue;
+            }
+            if (!isalnum(s[j])) {
+                j--;
+                continue;
+            }
+            if (tolower(s[i]) != tolower(s[j])) return false;
+            else {
+                i++;
+                j--;
+            } 
         }
         return true;
     }
