19518bd44b44ac5241b0ea9567508dfb5b072f6a
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-01-24 16:32:44 +0530

    Time: 13 ms (31.57%) | Memory: 7.6 MB (84.86%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,19 +1,14 @@
+// Recursive - Approach
+
 class Solution {
 public:
     int fib(int n) {
-        if (n == 0) return 0;
-        if (n == 1) return 1;
-        int fib1 = 0;
-        int fib2 = 1;
-        int newfib;
-        for (int i = 2; i <= n; i++) {
-            newfib = fib1 + fib2;
-            fib1 = fib2;
-            fib2 = newfib;
-        }
-        return newfib;
+        if (n <= 1) return n;
+        int last = fib(n - 1);
+        int slast = fib(n - 2);
+        return slast + last;
     }
 };
 
-// T.C => O(N)
+// T.C => O(n)
 // S.C => O(1)
