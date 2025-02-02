1df1f07b5ba0819bf49e1b2e25b05d3d390bb473
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-02-02 12:26:57 +0530

    Time: 0 ms (100.00%) | Memory: 27.4 MB (99.24%) - LeetSync

--- 3429-special-array-i/special-array-i.cpp
+++ 3429-special-array-i/special-array-i.cpp
@@ -1,15 +1,11 @@
-// Optimal - Solution =>
+// Optimal - Solution 1 (Bitwise - Operation Solution is more efficient, refer leetcode editorial)
 
 class Solution {
 public:
-    bool parity(int num) {
-        if (num % 2 == 0) return true;
-        else return false;
-    }
     bool isArraySpecial(vector<int>& nums) {
         if (nums.size() == 1) return true;
         for (int i = 0; i < nums.size() - 1; i++) {
-            if (parity(nums[i]) == parity(nums[i + 1])) return false;
+            if ((nums[i] % 2) == (nums[i + 1] % 2)) return false;
         }
         return true;
     }
