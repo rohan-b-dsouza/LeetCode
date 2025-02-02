acabf3ab4019930a0cfd18e6107574b38d83c03a
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-02-02 15:16:11 +0530

    Time: 32 ms (6.12%) | Memory: 29.6 MB (5.05%) - LeetSync

---
+++ 268-missing-number/missing-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        map <int, int> mpp;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            mpp[nums[i]]++;
+        }
+        for (int i = 0; i <= n; i++) {
+            if (mpp[i] == 0)
+            return i;
+        }
+        return -1;
+    }
+};
