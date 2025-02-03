4000cc3b372a549ab4368a18d0b35bef096653fa
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-02-03 19:43:48 +0530

    Time: 24 ms (6.82%) | Memory: 27.9 MB (5.02%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int xor1 = 0;
-        int xor2 = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            xor1 ^= nums[i];
-            xor2 ^= (i + 1);
+        unordered_map <int, int> mpp;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            mpp[nums[i]]++;
         }
-        return xor1 ^ xor2;
+        for (int i = 0; i <= n; i++) {
+            if (mpp[i] == 0)
+            return i;
+        }
+        return -1;
     }
 };
