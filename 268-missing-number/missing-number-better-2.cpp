e6aa1eb167a5cb3259a56b851cb43475e23e6fd2
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-02-03 19:44:19 +0530

    Create missing-number-better-2.cpp

---
+++ 268-missing-number/missing-number-better-2.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int xor1 = 0;
+        int xor2 = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            xor1 ^= nums[i];
+            xor2 ^= (i + 1);
+        }
+        return xor1 ^ xor2;
+    }
+};
