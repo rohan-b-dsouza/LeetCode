e3963c8a3655f1631841da47a281c507ba69da27
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-02-18 19:47:56 +0530

    Update sort-colors-brute.cpp

--- 75-sort-colors/sort-colors-brute.cpp
+++ 75-sort-colors/sort-colors-brute.cpp
@@ -1,7 +1,11 @@
+// Brute - Force Approach =>
+
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
         sort(nums.begin(), nums.end());
        }
-    }
 };
+
+// T.C => O(nlog(n))
+// S.C => O(1)
