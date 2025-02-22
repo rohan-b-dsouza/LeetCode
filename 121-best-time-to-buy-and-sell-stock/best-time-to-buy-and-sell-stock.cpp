69782ac121051c09b866e750a6cabf968d8b3493
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-02-22 23:32:28 +0530

    Time: 0 ms (100.00%) | Memory: 97.5 MB (25.50%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,16 +1,17 @@
+// Optimal - Approach =>
+
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int minbuy = INT_MAX;
+        int minbuy = prices[0];
         int maxProfit = 0;
-        for (int i = 0; i < prices.size(); i++) {
-            if (prices[i] < minbuy) {
-                minbuy = prices[i];
-            }
-            else {
-                maxProfit = max(maxProfit, prices[i] - minbuy);
-            }
+        for (int i = 1; i < prices.size(); i++) {
+            minbuy = min(minbuy, prices[i]);
+            maxProfit = max(maxProfit, prices[i] - minbuy);
         }
         return maxProfit;
     }
 };
+
+// Time Complexity :- O(n)
+// Space Complexity :- O(1)
