8a8e3050a2dd63f0b4b8d323139d00f34ddd68a4
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-02-25 18:45:18 +0530

    Create next-permutation-better.cpp

---
+++ 31-next-permutation/next-permutation-better.cpp
@@ -0,0 +1,11 @@
+// Better - Approach =>
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        next_permutation(nums.begin(), nums.end());
+    }
+};
+
+// T.C => O(N)
+// S.C => O(1)
