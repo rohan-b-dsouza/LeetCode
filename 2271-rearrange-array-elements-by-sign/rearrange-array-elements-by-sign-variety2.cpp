1769bee566606e6325092a8afd3467b3c074ea10
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-02-25 12:08:14 +0530

    Create rearrange-array-elements-by-sign-variety2.cpp

---
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign-variety2.cpp
@@ -0,0 +1,70 @@
+/*
+    LeetCode 2149 Variety 2->
+    Problem Statement:
+There’s an array ‘A’ of size ‘N’ with positive and negative elements (not necessarily equal). Without altering the relative order of positive and negative elements, you must return an array of alternately positive and negative values. The leftover elements should be placed at the very end in the same order as in array A.
+
+Note: Start the array with positive elements.
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector <int> alternateNumbers(vector <int> &a) {
+    vector <int> posi;
+    vector <int> nega;
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] > 0) {
+            posi.push_back(a[i]);
+        }
+        else{
+            nega.push_back(a[i]);
+        }
+    }
+    if (posi.size() > nega.size()) {
+        for (int i = 0; i < nega.size(); i++) {
+            a[2 * i] = posi[i];
+            a[2 * i + 1] = nega[i]; 
+        }
+        int idx = nega.size() * 2;
+        for (int i = nega.size(); i < posi.size(); i++) {
+            a[idx] = posi[i];
+            idx++;
+        }
+    }
+    else {
+        for (int i = 0; i < posi.size(); i++) {
+            a[2 * i] = posi[i];
+            a[2 * i + 1] = nega[i];
+        }
+        int idx = posi.size() * 2;
+        for (int i = posi.size(); i < nega.size(); i++) {
+            a[idx] = nega[i];
+            idx++;
+        }
+    }
+    return a;
+}
+
+int main() {
+    int n;
+    cout << "Enter the number of elements : ";
+    cin >> n;
+    vector <int> v;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        v.push_back(x);
+    }
+    cout << "Input : ";
+    for (int i = 0; i < n; i++) {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+    vector <int> ans = alternateNumbers(v);
+    cout << "Output : ";
+    for (int i = 0; i < n; i++) {
+        cout << ans[i] << " ";
+    }
+    return 0;
+}
