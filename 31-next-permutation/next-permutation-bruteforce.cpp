e571f41fdbbc940e360ac5a1f35fce1c53a9274a
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-02-25 18:49:19 +0530

    Update and rename next-permutation.cpp to next-permutation-bruteforce.cpp

---
+++ 31-next-permutation/next-permutation-bruteforce.cpp
@@ -0,0 +1,10 @@
+// BruteForce - Approach =>
+
+Step 1: Find all possible permutations of elements present and store them.
+
+Step 2: Search input from all possible permutations.
+
+Step 3: Print the next permutation present right after it.
+
+// T.C => O(N * N!) since total permutations are n! and each is of length n
+// S.C => O(N * N!) since an array will be used to store permutations and each is of length n
