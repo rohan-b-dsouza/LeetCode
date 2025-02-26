d34a11e218a08d238b48e4a9f52903782490ec45
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-02-26 17:26:12 +0530

    Time: 93 ms (29.61%) | Memory: 87.8 MB (39.51%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,19 +1,32 @@
+// Optimal - Approach => 
+// (Using unordered set and its find function which takes O(1) T.C)
+
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
         if (nums.empty()) return 0;
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
-        int cnt = 1;
-        int maxcnt = 1;
-        for (int i = 1; i < n; i++) {
-            if (nums[i] - nums[i - 1] == 1) {
-                cnt++;
-                maxcnt = max(maxcnt, cnt);
-            } else if (nums[i] != nums[i - 1]) {
-                cnt = 1;
+        unordered_set <int> st;
+        int longest = 1;
+        for (int i = 0; i < nums.size(); i++) {
+            st.insert(nums[i]);
+        }
+        for (auto it : st) {
+            if (st.find(it - 1) == st.end()) {
+                int cnt = 1;
+                int x = it + 1;
+                while (st.find(x) != st.end()) {
+                    cnt++;
+                    x++;
+                }
+                longest = max(longest, cnt);
             }
         }
-        return maxcnt;
+        return longest;
     }
 };
+
+// T.C => O(n) + O(2n) = O(3n) = O(n) ...{For the best and average case}
+// Note:- However, if the worst case can occur due to collisions then its better to use the 'Better' solution as using 
+// ordered set will also be inefficient}
+
+// S.C => O(n)
