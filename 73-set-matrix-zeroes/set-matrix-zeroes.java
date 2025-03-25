4e70fd571fa8003680d008d98e36c78f37bb688b
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-03-25 12:08:42 +0530

    Time: 1 ms (75.41%) | Memory: 45.8 MB (45.61%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,18 +1,28 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        HashSet <Integer> row = new HashSet<>();
-        HashSet <Integer> col = new HashSet<>();
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] tempMatrix = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                tempMatrix[i][j] = matrix[i][j]; 
+            }
+        }
+        for (int i = 0; i < m; i++) { 
+            for (int j = 0; j < n; j++) {
                 if (matrix[i][j] == 0) {
-                    row.add(i);
-                    col.add(j);
+                    for (int k = 0; k < n; k++) {
+                        tempMatrix[i][k] = 0; 
+                    }
+                    for (int k = 0; k < m; k++) {
+                        tempMatrix[k][j] = 0;
+                    }
                 }
             }
         }
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                if(row.contains(i) || col.contains(j)) matrix[i][j] = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = tempMatrix[i][j];
             }
         }
     }
