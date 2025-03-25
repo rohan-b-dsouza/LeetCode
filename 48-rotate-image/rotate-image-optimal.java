c86e1ebf25cc734bd64a9e8ed5115017b0a07f8b
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-03-25 19:56:05 +0530

    Update rotate-image-optimal.java

--- 48-rotate-image/rotate-image-optimal.java
+++ 48-rotate-image/rotate-image-optimal.java
@@ -1,16 +1,13 @@
 class Solution {
-    public void swap(int[][] matrix, int i, int j) {
-        int temp = matrix[i][j];
-        matrix[i][j] = matrix[j][i];
-        matrix[j][i] = temp;
-    }
 
     public void rotate(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
         for (int i = 0; i < m - 1; i++) {
             for (int j = i + 1; j < n; j++) {
-                swap(matrix, i, j);
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
         }
         for (int i = 0; i < m; i++) {
