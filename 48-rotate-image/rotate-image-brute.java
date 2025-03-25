4fc7f268346c549c08c0e4d4917cf7f82e36054e
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-03-25 18:45:37 +0530

    Update and rename rotate-image.java to rotate-image-brute.java

---
+++ 48-rotate-image/rotate-image-brute.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] temp = new int[matrix.length][matrix[0].length];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                temp[j][n - 1 - i] = matrix[i][j];
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = temp[i][j];
+            }
+        }
+    }
+}
