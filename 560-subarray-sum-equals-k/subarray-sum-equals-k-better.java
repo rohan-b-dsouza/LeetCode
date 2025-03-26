32bc067b848efc38aeaadfcfcb58878e7f2a071e
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-03-26 18:00:08 +0530

    Create subarray-sum-equals-k-better.java

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k-better.java
@@ -0,0 +1,21 @@
+// Optimal - Approach =>
+
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int n = nums.length;
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += nums[j];
+                if (sum == k) {
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+}
+
+// T.C => O(n^2)
+// S.C => O(1)
