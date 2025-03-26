1d84ce80520db145c418ba92380ab12d7f86d39f
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-03-26 18:18:17 +0530

    Update subarray-sum-equals-k-optimal.java

--- 560-subarray-sum-equals-k/subarray-sum-equals-k-optimal.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k-optimal.java
@@ -2,16 +2,15 @@
 
 class Solution {
     public int subarraySum(int[] nums, int k) {
+        HashMap <Integer, Integer> map = new HashMap<>();
         int cnt = 0;
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int sum = 0;
-        map.put(0, 1); // add zero initially to take care of edge case where map.get(sum - k) = 0
+        int preSum = 0;
+        map.put(0, 1); // handling edge case
         for (int i = 0; i < nums.length; i++) {
-            sum += nums[i];
-            if (map.containsKey(sum - k)) {
-                cnt += map.get(sum - k);
-            }
-            map.put(sum, map.getOrDefault(sum, 0) + 1);
+            preSum += nums[i];
+            int remove = preSum - k;
+            cnt += map.getOrDefault(remove, 0);
+            map.put(preSum, map.getOrDefault(preSum, 0) + 1);
         }
         return cnt;
     }
