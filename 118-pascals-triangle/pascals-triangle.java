6b1f8f12e4485e364e6e6ad67e8809c59d200d2d
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-03-28 17:38:09 +0530

    Time: 1 ms (84.41%) | Memory: 42.1 MB (39.79%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,18 +1,20 @@
-// Brute-Force Approach (Though this fails for larger value of numRows due to integer overflow while calculating factorial, as factorials grow quickly)
-
 class Solution {
+    public int ncr(int r, int c) {
+        long curr = 1;
+        for (int j = 0; j < c; j++) {
+            curr = (curr * (r - j)) / (j + 1);
+        }
+        return (int) curr;
+    }
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> listoflists = new ArrayList<>();
-        for (int i = 0; i < numRows; i++) {
-            List <Integer> sublist = new ArrayList<>();
-            int curr = 1;
-            for (int j = 0; j <= i; j++) {
-                sublist.add(curr);
-                curr = (curr* (i - j)) / (j + 1);
+        List <List<Integer>> listoflists = new ArrayList<>();
+        for (int r = 1; r <= numRows; r++) {
+            List<Integer> tempList = new ArrayList<>();
+            for (int c = 1; c <= r; c++) {
+                tempList.add(ncr(r - 1, c - 1));     
             }
-            listoflists.add(sublist);
+            listoflists.add(tempList);
         }
         return listoflists;
     }
-    
 }
