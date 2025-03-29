c50066f27669d11cdd67b95902f370be835b605c
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-03-29 15:15:58 +0530

    Create majority-element-ii-brute.java

---
+++ 229-majority-element-ii/majority-element-ii-brute.java
@@ -0,0 +1,26 @@
+// Brute-Force =>
+
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        List<Integer> majorityElement = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int cnt = 0;
+            int num = nums[i];
+            if (!majorityElement.contains(num)) { // check if num is a majority element only if its not already in the majorityElement list.
+                for (int j = 0; j < nums.length; j++) {
+                    if (nums[j] == num) {
+                        cnt++;
+                    }
+                }
+                if (cnt > (nums.length / 3)) {
+                    majorityElement.add(num);
+                }
+            }
+
+        }
+        return majorityElement;
+    }
+}
+
+// T.C => O(n^2)
+// S.C => O(2) => O(1)
