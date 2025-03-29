99acb78cc29746dc635521d114bd3c747262e16e
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-03-29 16:24:50 +0530

    Update majority-element-ii-better.java

--- 229-majority-element-ii/majority-element-ii-better.java
+++ 229-majority-element-ii/majority-element-ii-better.java
@@ -2,17 +2,13 @@
 
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
+        int n = nums.length;
         List<Integer> ans = new ArrayList<>();
         HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < n; i++) {
           map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-          if (map.get(nums[i]) > n / 3) ans.add(nums[i]);
+          if (map.get(nums[i]) > n / 3 && !ans.contains(nums[i])) ans.add(nums[i]);
           if (ans.size() == 2) break;
-        }
-        for (int i = 0; i < nums.length; i++) {
-            if ((map.get(nums[i]) > nums.length / 3) && !ans.contains(nums[i])) {
-                ans.add(nums[i]);
-            }
         }
         return ans;
     }
