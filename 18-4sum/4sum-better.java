4dc523586a20241e091d2524749fde3d82c43a85
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-04-08 22:44:22 +0530

    Create 4sum-better.java

---
+++ 18-4sum/4sum-better.java
@@ -0,0 +1,27 @@
+// Better - Approach =>
+
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        int n = nums.length;
+        HashSet<List<Integer>> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                HashSet<Integer> hash = new HashSet<>();
+                for (int k = j + 1; k < n; k++) {
+                    int fourth = target - (nums[i] + nums[j] + nums[k]);
+                    if (hash.contains(fourth)) {
+                        List<Integer> quad = Arrays.asList(nums[i], nums[j], nums[k], fourth);
+                        quad.sort(null);
+                        set.add(quad);
+                    }
+                    hash.add(nums[k]);
+                }
+            }
+        }
+        List<List<Integer>> ans = new ArrayList<>(set);
+        return ans;
+    }
+}
+
+// T.C => O(n^3)
+// S.C => O(x) + O(n) + O(x) => O(x + n) => O(max(n,x)) ---------{ here, the list 'ans' is used only to return the ans not to solve the problem therefore we dont consider it in the final space complexity)
