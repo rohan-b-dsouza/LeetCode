ad6c3380ad38ba5c5b8f88e6f6fd30c4168deea6
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-04-08 22:41:52 +0530

    Create 4sum-brute.java

---
+++ 18-4sum/4sum-brute.java
@@ -0,0 +1,27 @@
+// Brute - Force Approach => 
+
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        int n = nums.length;
+        HashSet <List<Integer>> set = new HashSet<>(); 
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    for (int l = k + 1; l < n; l++) {
+                        if (nums[i] + nums[j] + nums[k] + nums[l] == target) {
+                            List<Integer> quad = Arrays.asList(nums[i], nums[j], nums[k], nums[l]);
+                            quad.sort(null);
+                            set.add(quad);
+                        }
+                    }
+                }
+            }
+        }
+        List<List<Integer>> ans = new ArrayList<>(set);
+        return ans;
+    }
+}
+
+// T.C => O(n^4)
+// S.C => O(x) + O(n) => O(x) --> as the list 'ans' is used only for returning ans not to solve the problem.
+
