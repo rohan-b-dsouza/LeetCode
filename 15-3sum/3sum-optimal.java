1f7cd2836eb1512f785959a91d9799f1794ccd05
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-04-08 09:37:28 +0530

    Create 3sum-optimal.java

---
+++ 15-3sum/3sum-optimal.java
@@ -0,0 +1,40 @@
+// Optimal - Approach =>
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if(i != 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int j = i + 1;
+            int k = n - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum < 0) {
+                    j++;
+                } else if (sum > 0) {
+                    k--;
+                }
+                else {
+                    List<Integer> triplet = Arrays.asList(nums[i], nums[j], nums[k]);
+                    ans.add(triplet);
+                    j++;
+                    k--;
+                    while (j < k && nums[j] == nums[j-1]) {
+                        j++;
+                    }
+                    while (j < k && nums[k] == nums[k + 1]) {
+                        k--;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+}
+
+// T.C => O(n^2)
+// S.C => O(x) where x = no. of unique triplets
