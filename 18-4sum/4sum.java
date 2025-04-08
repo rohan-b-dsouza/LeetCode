046418baed7b6a164943a1ec7ef4ff3821b740a6
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-04-08 20:58:34 +0530

    Time: 15 ms (87.83%) | Memory: 43.9 MB (64.41%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,23 +1,37 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        int n = nums.length;
-        HashSet<List<Integer>> set = new HashSet<>();
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                HashSet<Long> hash = new HashSet<>();
-                for (int k = j + 1; k < n; k++) {
-                    long fourth = (long)target - nums[i] - nums[j] - nums[k];
-
-                    if (hash.contains(fourth)) {
-                        List<Integer> quad = Arrays.asList(nums[i], nums[j], nums[k], (int)fourth);
-                        quad.sort(null);
-                        set.add(quad);
+        int N = nums.length;
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < N; i++) {
+            if (i != 0 && nums[i] == nums[i - 1])
+                continue;
+            for (int j = i + 1; j < N; j++) {
+                if (j != i + 1 && nums[j] == nums[j - 1])
+                    continue;
+                int k = j + 1;
+                int l = N - 1;
+                while (k < l) {
+                    long sum = (long) nums[i] + nums[j] + nums[k] + nums[l];
+                    if (sum < target) {
+                        k++;
+                    } else if (sum > target) {
+                        l--;
+                    } else {
+                        List<Integer> quad = Arrays.asList(nums[i], nums[j], nums[k], nums[l]);
+                        ans.add(quad);
+                        k++;
+                        l--;
+                        while (k < l && nums[k] == nums[k - 1]) {
+                            k++;
+                        }
+                        while (k < l && nums[l] == nums[l + 1]) {
+                            l--;
+                        }
                     }
-                    hash.add((long)nums[k]);
                 }
             }
         }
-        List<List<Integer>> ans = new ArrayList<>(set);
         return ans;
     }
 }
