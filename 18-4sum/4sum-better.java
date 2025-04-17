a2e4a9f6bd171608ac49e5f70f29fc1a2ee7b8e9
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-04-17 21:13:05 +0530

    Update 4sum-better.java

--- 18-4sum/4sum-better.java
+++ 18-4sum/4sum-better.java
@@ -6,15 +6,16 @@ class Solution {
         HashSet<List<Integer>> set = new HashSet<>();
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
-                HashSet<Integer> hash = new HashSet<>();
+                HashSet<Long> hash = new HashSet<>();
                 for (int k = j + 1; k < n; k++) {
-                    int fourth = target - (nums[i] + nums[j] + nums[k]);
+                    long fourth = (long)target - nums[i] - nums[j] - nums[k];
+
                     if (hash.contains(fourth)) {
-                        List<Integer> quad = Arrays.asList(nums[i], nums[j], nums[k], fourth);
+                        List<Integer> quad = Arrays.asList(nums[i], nums[j], nums[k], (int)fourth);
                         quad.sort(null);
                         set.add(quad);
                     }
-                    hash.add(nums[k]);
+                    hash.add((long)nums[k]);
                 }
             }
         }
