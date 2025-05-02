bcc4857534400a2c44a7e51ce3ab45edc4b12253
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-05-02 18:46:55 +0530

    Update and rename reverse-pairs.java to reverse-pairs-optimal.java

--- 493-reverse-pairs/reverse-pairs-optimal.java
+++ 493-reverse-pairs/reverse-pairs-optimal.java
@@ -1,19 +1,18 @@
+// Optimal - Solution (Merge Sort)
+
 class Solution {
-    static int countPairs(int arr[], int low, int mid, int high) {
+    static int merge(int arr[], int low, int mid, int high) {
+        int left = low;
+        int right = mid + 1;
         int cnt = 0;
         int j = mid + 1;
+        List<Integer> list = new ArrayList<>();
         for (int i = low; i <= mid; i++) {
-            while (j <= high && arr[i] > 2 * (long)arr[j]) {
+            while (j <= high && arr[i] > 2L * arr[j]) {
                 j++;
             }
             cnt += j - (mid + 1);
         }
-        return cnt;
-    }
-    static void merge(int arr[], int low, int mid, int high) {
-        int left = low;
-        int right = mid + 1;
-        List<Integer> list = new ArrayList<>();
         while (left <= mid && right <= high) {
             if (arr[left] <= arr[right]) {
                 list.add(arr[left]);
@@ -34,23 +33,26 @@ class Solution {
         }
         for (int i = low; i <= high; i++) {
             arr[i] = list.get(i - low);
-        }    
-    }    
-
+        }
+        return cnt;
+        
+    }
     static int mergeSort(int arr[], int low, int high) {
         int cnt = 0;
         if (low >= high) return cnt;
         int mid = (low + high) / 2;
         cnt += mergeSort(arr, low, mid);
         cnt += mergeSort(arr, mid + 1, high);
-        cnt += countPairs(arr, low, mid, high);
-        merge(arr, low, mid, high);   
+        cnt += merge(arr, low, mid, high);   
         return cnt;
-    }
+    }    
 
     public int reversePairs(int[] nums) {
         int cnt = 0;
-        cnt = mergeSort(nums, 0, nums.length - 1);
+        cnt += mergeSort(nums, 0, nums.length - 1);
         return cnt;
     }
 }
+
+// T.C => O(n log (n)) + O(n log(n) = O(2* n * log(n)) -------------------{Explanation in notebook}
+// S.C => O(n) for the extra list
