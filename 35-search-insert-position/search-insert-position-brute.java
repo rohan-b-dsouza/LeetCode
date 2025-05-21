3e9410bd38e903d30537755de6a6f58bc2613b23
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-05-21 17:36:26 +0530

    Create search-insert-position-brute.java

---
+++ 35-search-insert-position/search-insert-position-brute.java
@@ -0,0 +1,13 @@
+// Bruteforce
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= target) return i;
+        }
+        return nums.length;
+    }
+}
+
+// T.C => O(n)
+// S.C => O(1)
