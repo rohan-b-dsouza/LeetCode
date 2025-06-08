e18db49a64d0a8c897ec8674ed47309d2bd68375
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-06-08 22:47:26 +0530

    Create search-in-rotated-sorted-array-ii-optimal.java

---
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii-optimal.java
@@ -0,0 +1,34 @@
+// Optimal 
+
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int low = 0;
+        int n = nums.length;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target)
+                return true;
+            if (nums[mid] == nums[low] && nums[low] == nums[high]) {
+                low++;
+                high--;
+            } else if (nums[mid] >= nums[low]) {
+                if (target < nums[mid] && target >= nums[low]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if ((target > nums[mid] && target <= nums[high])) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+}
+
+// T.C => O(log(n))
+// S.C => O(1)
