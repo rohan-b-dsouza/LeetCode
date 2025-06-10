397bc27cf587cc193fa4cbfee0f7fc14956d8b68
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-06-10 15:13:33 +0530

    Update and rename koko-eating-bananas.java to koko-eating-bananas-optimal.java

--- 907-koko-eating-bananas/koko-eating-bananas-optimal.java
+++ 907-koko-eating-bananas/koko-eating-bananas-optimal.java
@@ -35,29 +35,3 @@ class Solution {
 
 // T.C => O(log(max) * n) where max is maximum element in piles array and n is size of piles array
 // S.C => O(1)
-
-// Brute 
-
-// class Solution {
-//     public int minEatingSpeed(int[] piles, int h) {
-//         int n = piles.length;
-//         int max = Integer.MIN_VALUE;
-//         for (int i = 0; i < n; i++) {
-//             max = Math.max(max, piles[i]);
-//         }
-//         for (int i = 1; i <= max; i++) {
-//             int sum = 0;
-//             for (int j = 0; j < n; j++) {
-//                 sum += (int) Math.ceil((double) piles[j] / i);
-//                 if (sum > h)
-//                     break;
-//             }
-//             if (sum <= h)
-//                 return i;
-//         }
-//         return -1;
-//     }
-// }
-
-// T.C => O(max * n)
-// S.C => O(1)
