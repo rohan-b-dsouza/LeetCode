4760798cf88b3dc89e1990f9e8e9d835a8ce0871
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-06-10 10:30:39 +0530

    Time: 28 ms (51.97%) | Memory: 48.3 MB (51.45%) - LeetSync

---
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.java
@@ -0,0 +1,75 @@
+class Solution {
+  public int divisor(int[] nums, int mid, int threshold) {
+    int sum = 0;
+    for (int i = 0; i < nums.length; i++) {
+      sum += (int) Math.ceil((double) nums[i] / mid);
+      if (sum > threshold) return -1;
+    }
+    return 1;
+  }
+
+  public int smallestDivisor(int[] nums, int threshold) {
+    if (nums.length > threshold) return -1;
+    int max = Integer.MIN_VALUE;
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] > max) max = nums[i];
+    }
+    int low = 1;
+    int high = max;
+    int result = Integer.MAX_VALUE;
+    while (low <= high) {
+      int mid = low + (high - low) / 2;
+      if (divisor(nums, mid, threshold) == 1) {
+        result = Math.min(result, mid);
+        high = mid - 1;
+      }
+      else {
+        low = mid + 1;
+      }
+    }
+    if (result == Integer.MAX_VALUE) return -1;
+    return result;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//   public int smallestDivisor(int[] nums, int threshold) {
+//     int max = Integer.MIN_VALUE;
+//     for (int i = 0; i < nums.length; i++) {
+//         if (nums[i] > max) {
+//             max = nums[i];
+//         }
+//     }
+//     for (int i = 1; i <= max; i++) {
+//         int sum = 0;
+//         for (int j = 0; j < nums.length;j++) {
+//             sum += (int) Math.ceil((double) nums[j] / i);
+//             if (sum > threshold) break;
+//         }
+//         if (sum <= threshold) return i;
+//     }
+//     return -1;
+//   }
+// }
