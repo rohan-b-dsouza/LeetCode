d90113f223e536518295eec7ec078491de59654e
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-06-10 10:33:21 +0530

    Update and rename find-the-smallest-divisor-given-a-threshold.java to find-the-smallest-divisor-given-a-threshold-optimal.java

--- 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold-optimal.java
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold-optimal.java
@@ -1,3 +1,5 @@
+// Optimal 
+
 class Solution {
   public int divisor(int[] nums, int mid, int threshold) {
     int sum = 0;
@@ -32,44 +34,5 @@ class Solution {
   }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class Solution {
-//   public int smallestDivisor(int[] nums, int threshold) {
-//     int max = Integer.MIN_VALUE;
-//     for (int i = 0; i < nums.length; i++) {
-//         if (nums[i] > max) {
-//             max = nums[i];
-//         }
-//     }
-//     for (int i = 1; i <= max; i++) {
-//         int sum = 0;
-//         for (int j = 0; j < nums.length;j++) {
-//             sum += (int) Math.ceil((double) nums[j] / i);
-//             if (sum > threshold) break;
-//         }
-//         if (sum <= threshold) return i;
-//     }
-//     return -1;
-//   }
-// }
+// T.C => O(log(max) * n)
+// S.C => O(1)
