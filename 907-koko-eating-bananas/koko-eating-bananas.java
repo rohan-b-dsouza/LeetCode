f76939d38329e379c3d65cd10f7ecafe80c80890
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-06-10 15:12:53 +0530

    Time: 21 ms (49.49%) | Memory: 45.3 MB (35.26%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -0,0 +1,63 @@
+
+// Optimal
+
+class Solution {
+
+    public int sumofhours(int[] piles, int mid, int h) {
+        int sum = 0;
+        for (int i = 0; i < piles.length; i++) {
+            sum += (int) Math.ceil((double) piles[i] / mid);
+            if (sum > h)
+                return -1;
+        }
+        return 1;
+    }
+
+    public int minEatingSpeed(int[] piles, int h) {
+        int n = piles.length;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            max = Math.max(max, piles[i]);
+        }
+        int low = 1;
+        int high = max;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (sumofhours(piles, mid, h) == 1) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+}
+
+// T.C => O(log(max) * n) where max is maximum element in piles array and n is size of piles array
+// S.C => O(1)
+
+// Brute 
+
+// class Solution {
+//     public int minEatingSpeed(int[] piles, int h) {
+//         int n = piles.length;
+//         int max = Integer.MIN_VALUE;
+//         for (int i = 0; i < n; i++) {
+//             max = Math.max(max, piles[i]);
+//         }
+//         for (int i = 1; i <= max; i++) {
+//             int sum = 0;
+//             for (int j = 0; j < n; j++) {
+//                 sum += (int) Math.ceil((double) piles[j] / i);
+//                 if (sum > h)
+//                     break;
+//             }
+//             if (sum <= h)
+//                 return i;
+//         }
+//         return -1;
+//     }
+// }
+
+// T.C => O(max * n)
+// S.C => O(1)
