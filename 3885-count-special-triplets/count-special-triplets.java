becf6eac979f6543141923996f5043dceb02fa3d
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-06-15 16:11:58 +0530

    Time: 303 ms (100.00%) | Memory: 62.3 MB (100.00%) - LeetSync

---
+++ 3885-count-special-triplets/count-special-triplets.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int specialTriplets(int[] nums) {
+        HashMap<Integer, Integer> leftFreq = new HashMap<>();
+        HashMap<Integer, Integer> rightFreq = new HashMap<>();
+        int n = nums.length;
+        int mod = 1000000007;
+        for (int i = 0; i < n; i++) {
+            rightFreq.put(nums[i], rightFreq.getOrDefault(nums[i], 0) + 1);
+        }
+        long cnt = 0;
+        for (int j = 0; j < n; j++) {
+            int mid = nums[j];
+            rightFreq.put(mid, rightFreq.get(mid) - 1);
+            int left = leftFreq.getOrDefault(2 * mid, 0);
+            int right = rightFreq.getOrDefault(2 * mid, 0);
+            cnt +=  (long) left * right;
+            leftFreq.put(mid, leftFreq.getOrDefault(mid, 0) + 1);
+        }
+        return (int) (cnt % mod);
+    }
+
+}
