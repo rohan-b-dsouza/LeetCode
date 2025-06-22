804b072abd2fb8848ddbbb14808bd54c9a2e9f4a
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-06-22 16:54:20 +0530

    Time: 2 ms (74.82%) | Memory: 43.5 MB (6.46%) - LeetSync

--- 3914-check-if-any-element-has-prime-frequency/check-if-any-element-has-prime-frequency.java
+++ 3914-check-if-any-element-has-prime-frequency/check-if-any-element-has-prime-frequency.java
@@ -2,27 +2,22 @@ class Solution {
     public boolean isPrime(int n) {
         if (n == 1)
             return false;
-        boolean flag = false;
         for (int i = 2; i <= n / i; i++) {
             if (n % i == 0) {
-                flag = true;
-                break;
+                return false;
             }
         }
-        if (!flag)
-            return true;
-        return false;
+        return true;
     }
 
     public boolean checkPrimeFrequency(int[] nums) {
         int n = nums.length;
-        HashMap<Integer, Integer> freq = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            freq.put(nums[i], freq.getOrDefault(nums[i], 0) + 1);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        for (Map.Entry<Integer, Integer> entry : freq.entrySet()) {
-            if (isPrime(entry.getValue()))
-                return true;
+        for (int freq : map.values()) {
+            if (isPrime(freq)) return true;
         }
         return false;
     }
