c8ee5ca56d773bbdeb0d9121a9c3ea6c17aaf939
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-06-22 17:03:22 +0530

    Update and rename check-if-any-element-has-prime-frequency.java to check-if-any-element-has-prime-frequency-optimal.java

--- 3914-check-if-any-element-has-prime-frequency/check-if-any-element-has-prime-frequency-optimal.java
+++ 3914-check-if-any-element-has-prime-frequency/check-if-any-element-has-prime-frequency-optimal.java
@@ -1,3 +1,5 @@
+// Optimal
+
 class Solution {
     public boolean isPrime(int n) {
         if (n == 1)
@@ -22,3 +24,6 @@ class Solution {
         return false;
     }
 }
+
+// T.C => O(n + u * sqrt(f)) ------------{n is no. of elements in input array, u is no. of unique elements and f is freq of an unique element}
+// S.C => O(N) where N is no. of distinct elements in the input array
