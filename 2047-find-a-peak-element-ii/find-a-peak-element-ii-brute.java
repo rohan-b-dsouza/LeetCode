5d089709d59540ccf6bfe30481e3b88a57c3f0c8
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-06-22 20:02:39 +0530

    Update find-a-peak-element-ii-brute.java

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii-brute.java
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii-brute.java
@@ -1,11 +1,13 @@
 // Brute
 
+// (Using the fact that max element will definitely be a peak element too as no adjacent elements are equal (mentioned in problem))
+
 class Solution {
     public int[] findPeakGrid(int[][] mat) {
       int n = mat.length;
       int m = mat[0].length;
       int max = Integer.MIN_VALUE;
-      int resultIdx[] = new int[2];
+      int resultIdx[] = new int[2]; 
       for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             if (mat[i][j] > max) {
