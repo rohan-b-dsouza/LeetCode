5cee1cfcac9db69f73f495d7da134f684f6f5b5c
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-06-23 12:41:30 +0530

    Update powx-n-optimal.java

--- 50-powx-n/powx-n-optimal.java
+++ 50-powx-n/powx-n-optimal.java
@@ -1,58 +1,28 @@
 // Optimal (Recursive)
 
 class Solution {
-  public double power(double x, long n) {
-    if (n == 0) return 1; // base condition
+    public double power(double x, long n) {
+        if (n == 0)
+            return 1; // base condition
 
-    if (n == 1) return x; // anything raised to 1 is itself
+        if (n == 1)
+            return x; // anything raised to 1 is itself
 
-    if (n % 2 == 0) { // if n is even
-      return power(x * x, n / 2); // recursive call : x*x, n / 2
+        if (n % 2 == 0) { // if n is even
+            return power(x * x, n / 2); // recursive call : x*x, n / 2
+        }
+        // if n is odd
+        return x * power(x, n - 1); // recursive call x * power(x, n - 1);
     }
-    // if n is odd
-    return x * power(x, n - 1); // recursive call x * power(x, n - 1);
-  }
 
-  public double myPow(double x, int n) {
-    if (n < 0) {
-      return 1 / power(x,  - (long)n);
+    public double myPow(double x, int n) {
+        if (n < 0) {
+            return 1 / power(x, -(long) n); // if n = -2,147,483,648, its -n = 2,147,483,648 which overflows int therefore use long
+        }
+        return power(x, n);
     }
-    return power(x, n);
-  }
 }
+
 
 // T.C => O(log (n)) as in almost each step we do '/ 2'
 // S.C => O(log (n)) due to recursive call stack depth
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class Solution {
-//     public double myPow(double x, int n) {
-//         if (n == 0 || x == 1) return 1; // base case : any no to the power 0 is itself and 1 raised to anything is 1
-//         long temp = n; 
-//         if (n < 0) { // handle negative exponents
-//             x = 1 / x;
-//             temp = -temp; // if n = -2,147,483,648, its -n = 2,147,483,648 which overflows int therefore use long
-//         }
-//         double ans = 1;
-//         for (long i = 0; i < temp; i++) {
-//             ans *= x;
-//         }
-//     return ans;
-//     }
-// }
