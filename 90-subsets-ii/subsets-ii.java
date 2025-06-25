f33bba4b3e95a0546950b601f0f58e134eac3b5d
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-06-26 00:51:28 +0530

    Time: 2 ms (30.53%) | Memory: 43.5 MB (78.10%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int upperBound(int[] arr, int target) {
+        
+        int ans = arr.length;
+        int low = 0;
+        int high = arr.length - 1;
+        while (low <= high) {
+        int mid = (low + high) / 2;
+
+        if (arr[mid] > target) {
+            ans = mid;
+            
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1; 
+    }
+        
+        
+   }
+        return ans;
+    }
+    public void getPowerSet(int index, List<Integer> subset, List<List<Integer>> ans,  int n, int[] nums) {
+      if (index == n) { // Add subset to the ans list
+        ans.add(new ArrayList<>(subset));
+        return;
+      }
+      // Recursively include current element in the subset
+      subset.add(nums[index]); 
+      getPowerSet(index + 1, subset, ans, n, nums);
+      // Backtrack by removing last added element
+      subset.remove(subset.size() -1 );
+      // Recursively exclude current element from the subset
+      // Use upperBound to skip duplicates in order to ensure unique subsets in final 'ans' list
+      getPowerSet(upperBound(nums, nums[index]), subset, ans, n, nums);
+    }
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+      Arrays.sort(nums);
+      int n = nums.length;
+      List<List<Integer>> ans = new ArrayList<>();
+      List<Integer> subset = new ArrayList<>();
+      getPowerSet(0, subset, ans, n, nums);
+      return ans;
+    }
+}
