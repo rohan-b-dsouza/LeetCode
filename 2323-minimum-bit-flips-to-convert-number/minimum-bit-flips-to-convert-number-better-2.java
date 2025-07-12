191eb561bdeff3e27b9a456174772aa34bcaeebf
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-07-12 13:09:58 +0530

    Update and rename minimum-bit-flips-to-convert-number.java to minimum-bit-flips-to-convert-number-better-2.java

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number-better-2.java
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number-better-2.java
@@ -1,3 +1,5 @@
+// Better - 2 (Better than Better - 2)
+
 class Solution {
     public int minBitFlips(int start, int goal) {
         int ans = start ^ goal;
@@ -9,3 +11,6 @@ class Solution {
         return cntBits;
     }
 }
+
+// T.C => O(k) where k is no. of set bits
+// S.C => O(1)
