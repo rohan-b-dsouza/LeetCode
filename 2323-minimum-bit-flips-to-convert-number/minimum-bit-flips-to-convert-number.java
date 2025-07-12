23c4deda6ecf9330934953e9811a98ec3a053c49
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-07-12 13:16:09 +0530

    Update minimum-bit-flips-to-convert-number.java

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.java
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.java
@@ -1,6 +1,11 @@
+// Optimal
+
 class Solution {
     public int minBitFlips(int start, int goal) {
         int ans = start ^ goal;
         return Integer.bitCount(ans);
     }
 }
+
+// T.C => O(1)
+// S.C => O(1)
