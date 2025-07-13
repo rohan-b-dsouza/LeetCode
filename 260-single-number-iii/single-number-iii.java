873ae750aac4848f548142a53a2f8b13f8f2a0c6
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-07-13 11:46:29 +0530

    Time: 1 ms (99.74%) | Memory: 45.7 MB (65.07%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.java
@@ -0,0 +1,24 @@
+class Solution {    
+    public int[] singleNumber(int[] nums) { 
+        int n = nums.length;       
+        int zeroes = 0;
+        int ones = 0;
+        int xor = 0;
+        for (int i = 0; i < n; i++) {
+            xor ^= nums[i];
+        }
+        int rightmostSetBit = xor & ~(xor - 1);
+        for (int i = 0; i < n; i++) {
+            if ((nums[i] & rightmostSetBit) != 0) {
+                ones ^= nums[i];
+            }
+            else {
+                zeroes ^= nums[i];
+            }
+        }
+        if (ones < zeroes) {
+            return new int[] {ones, zeroes};
+        }
+        return new int[] {zeroes, ones};
+    }
+}
