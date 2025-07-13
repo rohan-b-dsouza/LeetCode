d7c6d6cc75ca521703e6b2431a6535e43833bfa2
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-07-13 09:32:24 +0530

    Time: 6 ms (15.85%) | Memory: 45.8 MB (19.96%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int n = nums.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+        for (int key : map.keySet()) {
+            if (map.get(key) == 1) return key;
+        }
+        return -1;
+    }
+}
