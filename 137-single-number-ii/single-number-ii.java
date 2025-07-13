3a38534605b190363a65ff14034a5ccc082231e8
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-07-13 09:35:58 +0530

    Time: 3 ms (65.35%) | Memory: 46 MB (12.12%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int n = nums.length;
+        int ans = 0;
+        for (int i = 0; i <= 31; i++) {
+            int cnt = 0;
+            for (int j = 0; j < n; j++) {
+                if ((nums[j] & (1 << i)) != 0) {
+                    cnt++;
+                }
+            }
+            if (cnt % 3 != 0) {
+                ans = ans | (1 << i);
+            }
+        }
+        return ans;
+    }
+}
