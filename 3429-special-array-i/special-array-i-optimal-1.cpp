a56dffc3b5856f59cdfe833a0601396408272954
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-07-20 21:53:02 +0530

    Update special-array-i-optimal-1.cpp

--- 3429-special-array-i/special-array-i-optimal-1.cpp
+++ 3429-special-array-i/special-array-i-optimal-1.cpp
@@ -1,15 +1,15 @@
-// Optimal - Solution 1 :  Modulo Comparisons
-// (Bitwise - Operation Solution is more efficient, refer leetcode editorial)
+// Optimal - 1 
+// (Bitwise solution is more efficient (Optimal - 2))
 
 class Solution {
-public:
-    bool isArraySpecial(vector<int>& nums) {
-        for (int i = 0; i < nums.size() - 1; i++) {
-            if ((nums[i] % 2) == (nums[i + 1] % 2)) return false;
+    public boolean isArraySpecial(int[] nums) {
+        int n = nums.length;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] % 2 == nums[i - 1] % 2) return false;
         }
         return true;
     }
-};
+}
 
 // T.C => O(n)
 // S.C => O(1)
