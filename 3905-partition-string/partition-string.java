4149f3fece724fffc549fde04e67ab03d3041bf3
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-07-20 17:36:48 +0530

    Time: 181 ms (49.48%) | Memory: 56.1 MB (27.81%) - LeetSync

--- 3905-partition-string/partition-string.java
+++ 3905-partition-string/partition-string.java
@@ -1,14 +1,14 @@
 class Solution {
     public List<String> partitionString(String s) {
-        String str = "";
+        StringBuilder str = new StringBuilder();
         HashSet<String> hs = new HashSet<>();
         List<String> ans = new ArrayList<>();
         for (int i = 0; i < s.length(); i++) {
-            str += s.charAt(i);
-            if (!hs.contains(str)) {
-                hs.add(str);
-                ans.add(str);
-                str = "";
+            str.append(s.charAt(i));
+            if (!hs.contains(str.toString())) {
+                hs.add(str.toString());
+                ans.add(str.toString());
+                str.setLength(0);
             }
         }
         return ans;
