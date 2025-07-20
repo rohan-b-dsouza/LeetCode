14722b26980e50a4b073e98d8844c7b99e7e528c
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-07-20 12:54:36 +0530

    Update count-special-triplets-brute.java

--- 3885-count-special-triplets/count-special-triplets-brute.java
+++ 3885-count-special-triplets/count-special-triplets-brute.java
@@ -3,18 +3,18 @@
 class Solution {
     public int specialTriplets(int[] nums) {
         int n = nums.length;
-        int cnt = 0;
+        long cnt = 0;
         int mod = 1000000007;
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
                 for (int k = j + 1; k < n; k++) {
-                    if ((nums[i] == nums[j] * 2) && (nums[k] == nums[j] * 2)) {
+                    if ((nums[i] == (long) nums[j] * 2) && (nums[k] == (long)nums[j] * 2)) {
                         cnt++;
                     }
                 }
             }
         }
-        return (cnt % mod);
+        return (int)(cnt % mod);
     }
 
 }
