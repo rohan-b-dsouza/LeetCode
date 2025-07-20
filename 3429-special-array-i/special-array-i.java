b39f77c53448f7dad8be551eba748bccbc1c7f31
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-07-20 22:00:50 +0530

    Update special-array-i.java

--- 3429-special-array-i/special-array-i.java
+++ 3429-special-array-i/special-array-i.java
@@ -1,9 +1,14 @@
+// Optimal - 2 (Bitwise AND)
+
 class Solution {
     public boolean isArraySpecial(int[] nums) {
         int n = nums.length;
         for (int i = 1; i < n; i++) {
-            if (nums[i] % 2 == nums[i - 1] % 2) return false;
+            if ((nums[i] & 1) == (nums[i - 1] & 1)) return false;
         }
         return true;
     }
 }
+
+// T.C => O(n)
+// S.C => O(1)
