74ca1aef259ccebdc22114b2858645c27db6e078
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-07-21 11:28:41 +0530

    Time: 38 ms (67.62%) | Memory: 96.9 MB (78.92%) - LeetSync

---
+++ 3886-count-number-of-trapezoids-i/count-number-of-trapezoids-i.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int countTrapezoids(int[][] points) {
+        final int MOD = 1_000_000_007;
+        // Map for mapping (key : y, value : freq(y))
+        HashMap <Integer, Integer> yfreq = new HashMap<>();
+        // Map frequency of every 'y' coordinate in the map 
+        for (int i = 0; i < points.length; i++) {
+            int y = points[i][1];
+            yfreq.put(y, yfreq.getOrDefault(y, 0) + 1);
+        }
+        long total = 0;
+
+        for (int freq : yfreq.values()) {
+            total += ((long) freq * (freq - 1)) / 2;
+        }
+        long trapezoids = 0;
+        for (int freq : yfreq.values()) {
+            long pairCount = ((long) freq * (freq - 1)) / 2;
+            total -=  pairCount;
+            trapezoids = (trapezoids + (total * pairCount)) ;
+        }
+        return (int) (trapezoids % MOD);
+    }
+}
