a52afe9d6a0ec8e1c8558cb2f9e4184e5181cbf8
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-07-27 23:58:57 +0530

    Time: 4 ms (10.98%) | Memory: 44.9 MB (18.08%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -0,0 +1,41 @@
+/*Definition of singly linked list:
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+        val = 0;
+        next = null;
+    }
+
+    ListNode(int data1) {
+        val = data1;
+        next = null;
+    }
+
+    ListNode(int data1, ListNode next1) {
+        val = data1;
+        next = next1;
+    }
+}
+ */
+
+class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) return false;
+        // Hashset to store visited nodes
+        HashSet<ListNode> st = new HashSet<>();
+        ListNode temp = head;
+        while (temp != null) {
+            // If visited node appears, cycle exists return true
+            if (st.contains(temp)) {
+                return true;
+            }
+            // Store current node to the HashSet
+            st.add(temp);
+            temp = temp.next;
+        }
+        // If no cycle detected, return false
+        return false;
+    }
+}
