ec41f0c09281cb78312806ecd62daf0c0810f9ee
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-07-27 23:25:21 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (27.22%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -0,0 +1,39 @@
+/*Definition of singly linked list:
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+        val = 0;
+        next = null;
+    }
+
+    ListNode(int data1) {
+        val = data1;
+        next = null;
+    }
+
+    ListNode(int data1, ListNode next1) {
+        val = data1;
+        next = next1;
+    }
+}
+ */
+
+class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) return false;
+        // Initialize two pointers fast and slow
+        ListNode fast = head;
+        ListNode slow = head;
+        // Traverse the LL with the fast and slow pointers
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next; // Move fast by two step
+            slow = slow.next; // Move slow by one step
+            // Check if slow and fast pointers meet
+            if (fast == slow) return true; // Loop is detected
+        }
+        // If fast reaches end of loop or null, there is no loop
+        return false;
+    }
+}
