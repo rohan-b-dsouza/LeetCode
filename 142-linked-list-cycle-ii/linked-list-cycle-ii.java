cb6f4eb645fb5cb5453f7a8b4ead21059e00403e
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-07-28 20:57:48 +0530

    Update linked-list-cycle-ii.java

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -1,3 +1,5 @@
+// Optimal
+
 /**
  * Definition for singly-linked list.
  * class ListNode {
@@ -14,18 +16,24 @@ public class Solution {
         if (head == null) return null;
         ListNode slow = head;
         ListNode fast = head;
+        // Traverse till fast is not null or fast is not at last node
         while (fast != null && fast.next != null) {
             fast = fast.next.next;
             slow = slow.next;
+            // If fast and slow meet it means that cycle exists, hence set slow to head and move fast and slow pointer by one step till they meet again
             if (fast == slow) {
                 slow = head;
                 while (fast != slow) {
                     fast = fast.next;
                     slow = slow.next;
                 }
+                // Return the starting point of loop
                 return slow;
             }
         }
         return null;
     }
 }
+
+// T.C => O(n)
+// S.C => O(1)
