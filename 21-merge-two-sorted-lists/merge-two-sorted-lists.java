9e8241d9990751b84f27441e01c6383efe4c27f2
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-07-29 18:12:00 +0530

    Update merge-two-sorted-lists.java

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -1,3 +1,5 @@
+// Brute
+
 /*Definition of singly linked list:
 class ListNode {
     int val;
@@ -24,23 +26,32 @@ class Solution {
   public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
     ListNode temp = list1;
     List<Integer> list = new ArrayList<>();
+      // Add elements in list1 to list
     while (temp != null) {
       list.add(temp.val);
       temp = temp.next;
     }
     temp = list2;
+      // Add elements in list2 in list
     while (temp != null) {
         list.add(temp.val);
         temp = temp.next;
     }
+      // Sort list
     Collections.sort(list);
     ListNode dummyNode = new ListNode(-1);
     temp = dummyNode;
+      // Build a linked list from the list using a dummynode
     for (int num : list) {
         temp.next = new ListNode(num);
         temp = temp.next;
     }
+      // Return the head of the newly created list
     return dummyNode.next;
 
   }
 }
+
+
+// T.C => O(N) + O(N * log(N)) + O(N) -------{where N = n + m}
+// S.C => O(N) + O(N) for the extra ArrayList and Linked List
