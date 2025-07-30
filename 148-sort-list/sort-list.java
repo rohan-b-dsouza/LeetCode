bccf9ecc6187de311ba388ff796df7f2d842e67d
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-07-30 14:29:13 +0530

    Time: 13 ms (25.95%) | Memory: 55.2 MB (93.24%) - LeetSync

---
+++ 148-sort-list/sort-list.java
@@ -0,0 +1,43 @@
+/*Definition of singly linked list:
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+        val = 0;
+        next = null;
+    }
+
+    ListNode(int data1) {
+        val = data1;
+        next = null;
+    }
+
+    ListNode(int data1, ListNode next1) {
+        val = data1;
+        next = next1;
+    }
+}
+ */
+
+class Solution {
+    public ListNode sortList(ListNode head) {
+        List<Integer> list = new ArrayList<>();
+        ListNode temp = head;
+        // Store all elements of LL into a list
+        while (temp != null) {
+            list.add(temp.val);
+            temp = temp.next;
+        }
+        // Sort the list
+        list.sort(null);
+        temp = head;
+        int cnt = 0;
+        // Overwrite LL with these sorted node values
+        while (temp != null) {
+            temp.val = list.get(cnt++);
+            temp = temp.next;
+        }
+        return head;
+    }
+}
