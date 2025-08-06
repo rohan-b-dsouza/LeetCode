2b5ff2363e96d738986e304cfc7f241a63ed465b
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-08-06 17:43:41 +0530

    Time: 5 ms (29.09%) | Memory: 42.9 MB (5.74%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -0,0 +1,25 @@
+class Solution {
+  public void getLetterCombinations(
+      int index, String combination, String[] map, List<String> ans, String digits) {
+    if (index == digits.length()) { // Base case: if index reaches the end of digits
+      ans.add(combination); // Add the current combination to the answer
+      return;
+    }
+    // Get string corresponding to the current digit
+    String s = map[digits.charAt(index) - '0'];
+    // Loop through the corresponding characters
+    for (int i = 0; i < s.length(); i++) {
+      // Recursively call the function with next index
+      // Add current character to the string
+      getLetterCombinations(index + 1, combination + s.charAt(i), map, ans, digits);
+    }
+  }
+
+  public List<String> letterCombinations(String digits) {
+    List<String> ans = new ArrayList<>();
+    if (digits.length() == 0) return ans;
+    String[] map = new String[] {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    getLetterCombinations(0, "", map, ans, digits);
+    return ans;
+  }
+}
