117bff4ce1f67cfd31e776d7e4a99141a2b56c10
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-08-11 16:42:41 +0530

    Update range-product-queries-of-powers-brute.java

--- 2529-range-product-queries-of-powers/range-product-queries-of-powers-brute.java
+++ 2529-range-product-queries-of-powers/range-product-queries-of-powers-brute.java
@@ -1,7 +1,9 @@
 // Brute 
 
 class Solution {
+    // Storing modulo 10^9 + 7
     private static final int MOD = 1_000_000_007;
+    // Calculating product of powers list from left to right
     public int calcPow(int left, int right, List<Integer> powers) {
         long power = 1;
         for (int i = left; i <= right; i++) {
@@ -10,14 +12,21 @@ class Solution {
         return (int) power;
     }
     public int[] productQueries(int n, int[][] queries) {
+        int size = queries.length;
+        // List to store the powers
         List<Integer> powers = new ArrayList<>();
+        // Array to store the answer of products
         int[] ans = new int[size];
+        // Finding the powers
         while (n > 0) {
+            // Find rightmost set bit of n, its also the power of 2 hence add to powers
             int rightmostSetBit = n & -n;
             powers.add(rightmostSetBit);
+            // Remove the rightmost set bit to take the next power of 2 in next iteration
             n = n & (n - 1);
         }
         int i = 0;
+        // For each query, compute the product of powers from left to right
         for (int[] query : queries) {
             int power = calcPow(query[0], query[1], powers);
             ans[i++] = power;
