3d4a92227dd8a4897d9bb63beaa48ebb5198f575
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-08-11 16:42:26 +0530

    Update and rename range-product-queries-of-powers.java to range-product-queries-of-powers-brute.java

--- 2529-range-product-queries-of-powers/range-product-queries-of-powers-brute.java
+++ 2529-range-product-queries-of-powers/range-product-queries-of-powers-brute.java
@@ -1,3 +1,5 @@
+// Brute 
+
 class Solution {
     private static final int MOD = 1_000_000_007;
     public int calcPow(int left, int right, List<Integer> powers) {
@@ -8,7 +10,6 @@ class Solution {
         return (int) power;
     }
     public int[] productQueries(int n, int[][] queries) {
-        int size = queries.length;
         List<Integer> powers = new ArrayList<>();
         int[] ans = new int[size];
         while (n > 0) {
@@ -24,3 +25,6 @@ class Solution {
         return ans;
     }
 }
+
+// T.C => O(no. of set bits in n) + O(m * (right - left + 1)) where m is size of query array
+// S.C => O(no. of powers) + O(m) where m is size of query array (ans array will always be of size m as mentioned in qn)
