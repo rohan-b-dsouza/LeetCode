bbc13cc4bb25cff6764b67fb65149378918a1a9b
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-08-22 10:06:23 +0530

    Time: 2 ms (83.30%) | Memory: 45 MB (19.88%) - LeetSync

---
+++ 39-combination-sum/combination-sum.java
@@ -0,0 +1,30 @@
+
+class Solution {
+    public void getCombinationsOfSumTarget(int index, List<Integer> combination, int target, int n, int[] candidates,
+            List<List<Integer>> ans) {
+        if (target == 0) { // Base Case : If valid combination is found add to ans list
+            ans.add(new ArrayList<>(combination));
+            return;
+        }
+        if (target < 0 || index == n) { // no valid combination found
+            return;
+        }
+        // Recursive Case : Include current element and decrement from target, and recurse on same index
+        // (Left Recursion Tree)
+        combination.add(candidates[index]);
+        getCombinationsOfSumTarget(index, combination, target - candidates[index], n, candidates, ans);
+        // Backtrack by removing the last added element
+        // (Right Recursion Tree) Recursively call the function by excluding the element
+        combination.remove(combination.size() - 1);
+
+        getCombinationsOfSumTarget(index + 1, combination, target, n, candidates, ans);
+    }
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        int n = candidates.length;
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        getCombinationsOfSumTarget(0, list, target, n, candidates, ans);
+        return ans;
+    }
+}
