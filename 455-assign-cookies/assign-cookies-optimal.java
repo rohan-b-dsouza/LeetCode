0de995683a6e567d9848c50295b3c8f059216f2e
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-08-29 18:50:10 +0530

    Update assign-cookies-optimal.java

--- 455-assign-cookies/assign-cookies-optimal.java
+++ 455-assign-cookies/assign-cookies-optimal.java
@@ -4,10 +4,12 @@ class Solution {
     public int findContentChildren(int[] g, int[] s) {
         int n = g.length;
         int m = s.length;
+        // Sort both the arrays g and s
         Arrays.sort(g);
         Arrays.sort(s);
         int i = 0;
         int j = 0;
+        // Greedily assign cookies to students in such a way that you maximize the number of satisfied students without ever "wasting" a bigger cookie unnecessarily.
         while (i < n && j < m) {
             if (s[j] >= g[i]) {
                 i++;
