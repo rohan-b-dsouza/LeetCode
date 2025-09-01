c0b116e6cb6effb7f812e543c42a10c8421b7b6d
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-09-01 17:14:47 +0530

    Update maximum-average-pass-ratio-optimal.java

--- 1917-maximum-average-pass-ratio/maximum-average-pass-ratio-optimal.java
+++ 1917-maximum-average-pass-ratio/maximum-average-pass-ratio-optimal.java
@@ -37,6 +37,6 @@ class Solution {
     }
 }
 
-// T.C => O(n * log(n)) for building the PQ + O(extraStudents * log n) for the while loop + O(n) to iterate over the PQ
-// Overall T.C => O(n * log(n))
+// T.C => O(n * log(n)) for building the PQ + O(extraStudents * (2 * log (n)) for the while loop + O(n) to iterate over the PQ
+// Overall T.C => O((n + extraStudents) * log n)
 // S.C => O(2 * n) = O(n) for storing all the classes
