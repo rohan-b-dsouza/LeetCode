aab1c35b4e2a11042e410605c384b65253934a33
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-09-04 19:18:38 +0530

    Update find-closest-person-optimal.java

--- 3830-find-closest-person/find-closest-person-optimal.java
+++ 3830-find-closest-person/find-closest-person-optimal.java
@@ -2,9 +2,9 @@
 
 class Solution {
     public int findClosest(int x, int y, int z) {
-        // If x is closer to z return 1
+        // If x is more closer to z return 1
         if (Math.abs(x - z) < Math.abs(y - z)) return 1;
-        // If y is closer to z return 2
+        // If y more is closer to z return 2
         if (Math.abs(y - z) < Math.abs(x - z)) return 2;
         // If both are equally closer to z return 0
         return 0;
