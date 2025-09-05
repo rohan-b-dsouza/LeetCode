444f20799d8118390258faa134d67996a61cf4af
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-09-05 14:19:58 +0530

    Time: 2 ms (99.24%) | Memory: 45.4 MB (91.43%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,55 +1,30 @@
 class Solution {
-    // To calculate number of candies
     public int candy(int[] ratings) {
-        // Get number of children
-        int n = ratings.length;
-        // If no children, return 0
-        if (n == 0) return 0;
-
-        /*Initialize arrays to store candies
-        for each child from left and right*/
-        int[] left = new int[n];
-        int[] right = new int[n];
-        Arrays.fill(left, 1);
-        Arrays.fill(right, 1);
-
-        /*Left to right pass
-        Traverse the ratings from left to right
-        to adjust candies based on ratings*/
-        for (int i = 1; i < n; ++i) {
-            /*If the current child's rating is
-            higher than the previous one*/
-            if (ratings[i] > ratings[i - 1]) {
-                /*Give the current child one
-                more candy than the previous one*/
-                left[i] = left[i - 1] + 1;
-            }
+       int n = ratings.length;
+       int[] left = new int[n];
+       int[] right = new int[n];
+       left[0] = 1;
+       right[n - 1] = 1;
+       for (int i = 1; i < n; i++) {
+        if (ratings[i] > ratings[i - 1]) {
+            left[i] = left[i - 1] + 1;
         }
-
-        /*Right to left pass
-        Traverse the ratings from right to left
-        to adjust candies based on ratings*/
-        for (int i = n - 2; i >= 0; --i) {
-            /*If the current child's rating is
-            higher than the next one*/
-            if (ratings[i] > ratings[i + 1]) {
-                /*Give the current child one
-                more candy than the next one*/
-                right[i] = right[i + 1] + 1;
-            }
+        else {
+            left[i] = 1;
         }
-
-        /*Combine results
-        Calculate the total number of candies
-        needed by taking the maximum from
-        left and right for each child*/
-        int ans = 0;
-        for (int i = 0; i < n; ++i) {
-            /*Each child gets the maximum
-            candies from left and right*/
-            ans += Math.max(left[i], right[i]);
+       }
+       for (int i = n - 2; i >= 0; i--) {
+        if (ratings[i] > ratings[i + 1]) {
+            right[i] = right[i + 1] + 1;
         }
-        // Return total
-        return ans;
+        else {
+            right[i] = 1;
+        }
+       }
+       int sum = 0;
+       for (int i = 0; i < n; i++) {
+        sum += Math.max(left[i], right[i]);
+       }
+       return sum;
     }
 }
