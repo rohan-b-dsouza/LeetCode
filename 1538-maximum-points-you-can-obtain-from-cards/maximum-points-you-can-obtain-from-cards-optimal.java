571c12bbd4ee73afc8d991e2ee9bf3430cacb42f
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-09-06 18:06:43 +0530

    Update maximum-points-you-can-obtain-from-cards-optimal.java

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards-optimal.java
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards-optimal.java
@@ -4,7 +4,7 @@ class Solution {
     public int maxScore(int[] cardPoints, int k) {
         int n = cardPoints.length;
         int sum = 0;
-        // Step 1 : Calculate sum of first k elements from left
+        // Calculate sum of first k elements from left
         for (int i = 0; i < k; i++) {
           sum += cardPoints[i];
         }
