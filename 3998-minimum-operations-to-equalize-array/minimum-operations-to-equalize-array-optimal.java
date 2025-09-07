90d5b29adbb9905f153704561f036079dd04d59f
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-09-07 11:15:18 +0530

    Update and rename minimum-operations-to-equalize-array.java to minimum-operations-to-equalize-array-optimal.java

---
+++ 3998-minimum-operations-to-equalize-array/minimum-operations-to-equalize-array-optimal.java
@@ -0,0 +1,20 @@
+// Optimal
+
+class Solution {
+    public int minOperations(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n - 1; i++) {
+            // If all elements in the array are not equal, then we will require one operation to make all elements equal
+            // i.e Replace each element with the 'AND' of all elements ; taking subarray as entire array
+            if (nums[i] != nums[i + 1]) {
+                return 1;
+            }
+        }
+        // If all elements are equal, then no operations are required
+        return 0;
+        
+    }
+}
+
+// T.C => O(n)
+// S.C => O(1)
