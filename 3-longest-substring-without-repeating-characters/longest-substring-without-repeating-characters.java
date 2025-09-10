7720029a16e08398e96e8c58127fa759d0d49e51
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-09-10 17:28:36 +0530

    Time: 22 ms (10.28%) | Memory: 45.2 MB (14.58%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -0,0 +1,28 @@
+// Brute
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        int maxlen = 0;
+        for (int i = 0; i < n; i++) {
+            // Array to mark visited characters 
+            int[] hash = new int[256];
+            int len = 0;
+            // For each i, find the longest consecutive sequence of non - repeating characters
+            for (int j = i; j < n; j++) {
+                // If current character has not occured earlier, mark it visited and increment len
+                if (hash[s.charAt(j)] == 0) {
+                    hash[s.charAt(j)]++;
+                    len++;
+                }
+                // If current character is visited then break 
+                else {
+                    break;
+                }
+                // At each step update maxlen
+                maxlen = Math.max(maxlen, len);
+            }
+        }
+        return maxlen;
+    }
+}
