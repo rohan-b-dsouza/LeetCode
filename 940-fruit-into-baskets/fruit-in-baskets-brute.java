e78a32b28b91c9996ab7310c7ee867cbb8041d41
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-09-13 20:45:35 +0530

    Create fruit-in-baskets-brute.java

---
+++ 940-fruit-into-baskets/fruit-in-baskets-brute.java
@@ -0,0 +1,27 @@
+// Brute
+
+class Solution {
+    public int totalFruits(int[] fruits) {
+        int n = fruits.length;
+        int maxlen = 0;
+      // Generate all possible valid subarrays 
+        for (int i = 0; i < n; i++) {
+          // Set to store occurence of a kind of fruit
+            HashSet<Integer> st = new HashSet<>();
+          // Find the max possible valid subarray from current i 
+            for (int j = i; j < n; j++) {
+              // Add current fruit to set
+                st.add(fruits[j]);
+              // If fruit types exceed 2 break
+                if (st.size() > 2) break;
+              // Update maxlen
+                maxlen = Math.max(j - i + 1, maxlen);
+            }
+        }
+      // Return maxlen
+        return maxlen;
+    }
+}
+
+// T.C => O(n ^ 2)
+// S.C => O(1)
