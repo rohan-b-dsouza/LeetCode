0f7fcc7c4677740d63efb2164264abe94d7b143c
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-09-13 14:39:21 +0530

    Time: 4 ms (35.77%) | Memory: 53 MB (46.12%) - LeetSync

---
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -0,0 +1,33 @@
+// Optimal (Sliding Window)
+
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int n = nums.length;
+        // Initialize maxlen and zeroes to 0
+        int maxlen = 0;
+        int zeroes = 0;
+        // Initialize two pointers l and r to 0
+        int l = 0;
+        int r = 0;
+        // Traverse till r is within bounds 
+        while (r < n) {
+            // If current element is a 0, increment zeroes
+            if (nums[r] == 0) zeroes++;
+            // If zeroes exceed k, shrink the window by 1 using 'l' to limit the window to maxlen and try to find a larger possible substring
+            if (zeroes > k) {
+                // If a zero is found decrement zeroes
+                if (nums[l] == 0) zeroes--;
+                l++;
+            }
+            // Update maxlen
+            maxlen = Math.max(maxlen, r - l + 1);
+            // Expand the window using 'r' pointer
+            r++;
+        } 
+        // Return maxlen
+        return maxlen;
+    }
+}
+
+// T.C => O(2 * n)
+// S.C => O(1)
