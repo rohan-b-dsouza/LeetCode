808564e5429fffcdce8648bea169c5b615cec4f1
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-09-21 16:43:28 +0530

    Update and rename bitwise-or-of-even-numbers-in-an-array.java to bitwise-or-of-even-numbers-in-an-array-optimal.java

--- 4009-bitwise-or-of-even-numbers-in-an-array/bitwise-or-of-even-numbers-in-an-array-optimal.java
+++ 4009-bitwise-or-of-even-numbers-in-an-array/bitwise-or-of-even-numbers-in-an-array-optimal.java
@@ -1,12 +1,18 @@
+// Optimal (Bit Manipulation)
+
 class Solution {
     
     public int evenNumberBitwiseORs(int[] nums) {
         int n = nums.length;
         int or = 0;
         for (int i = 0; i < n; i++) {
+            // If number is even (i.e LSB = 0) 'OR' it with or
            if ((nums[i] & 1) == 0) or |= nums[i]; 
         }
         return or;
     }
     
 }
+
+// T.C => O(n)
+// S.C => O(1)
