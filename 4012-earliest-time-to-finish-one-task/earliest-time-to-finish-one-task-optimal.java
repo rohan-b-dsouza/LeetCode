61d9c180251dfd9af53871b43df6e14b18da49f8
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-09-21 17:02:20 +0530

    Update and rename earliest-time-to-finish-one-task.java to earliest-time-to-finish-one-task-optimal.java

--- 4012-earliest-time-to-finish-one-task/earliest-time-to-finish-one-task-optimal.java
+++ 4012-earliest-time-to-finish-one-task/earliest-time-to-finish-one-task-optimal.java
@@ -1,10 +1,16 @@
+// Optimal
+
 class Solution {
     public int earliestTime(int[][] tasks) {
         int min = Integer.MAX_VALUE;
         int n = tasks.length;
         for (int[] a : tasks) {
+            // At each step, update the minimum earliest time seen so far
             min = Math.min(min, a[0] + a[1]);
         }
         return min;
     }
 }
+
+// T.C => O(n)
+// S.C => O(1)
