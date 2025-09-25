4afae87ed583d8a96f523c6bda106028bf4a4aa9
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-09-25 14:00:59 +0530

    Time: 7 ms (69.76%) | Memory: 45.5 MB (68.26%) - LeetSync

---
+++ 3997-maximize-sum-of-at-most-k-distinct-elements/maximize-sum-of-at-most-k-distinct-elements.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int[] maxKDistinct(int[] nums, int k) {
+        // Use a TreeSet in descending order to store distinct numbers
+        TreeSet<Integer> set = new TreeSet<>(Collections.reverseOrder());
+        for (int num : nums) {
+            set.add(num);
+        }
+        
+        // Prepare the result array with at most k elements
+        int size = Math.min(k, set.size());
+        int[] res = new int[size];
+        int idx = 0;
+        for (int num : set) {
+            if (idx == size) break;
+            res[idx++] = num;
+        }
+        return res;
+    }
+}
