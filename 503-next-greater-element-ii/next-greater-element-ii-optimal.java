e325dd2c80776f8427a1675a9c588a0449978069
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-09-27 19:09:30 +0530

    Update and rename next-greater-element-ii.java to next-greater-element-ii-optimal.java

--- 503-next-greater-element-ii/next-greater-element-ii-optimal.java
+++ 503-next-greater-element-ii/next-greater-element-ii-optimal.java
@@ -1,19 +1,30 @@
+// Optimal
+
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
         int n = nums.length;
         Deque<Integer> stack = new ArrayDeque<>();
         int[] ans = new int[n];
+        // Imagine an hypothetical array (i.e double the current array but hypothetically)
         for (int i = 2 * n - 1; i >= 0; i--) {
+            // Get the actual index using modulo n
             int idx = i % n;
+            // If stack is not empty and current element is greater than stack's peek, then pop elements
             while (!stack.isEmpty() && nums[idx] >= stack.peek()) {
                 stack.pop();
             }
+            // If i < n, it means we are now in the actual array phase, hence now start updating 'ans' array
             if (i < n) {
                 if (stack.isEmpty()) ans[i] = -1;
                 else ans[i] = stack.peek();
             }
+            // Push current element to the stack
             stack.push(nums[idx]);
         } 
+        // Return the answer
         return ans;
     }
 }
+
+// T.C => O(2 * n) for outer loop and in worst case inner loop can also go upto O(2 * n) hence O(4 * n) = O(n)
+// S.C => O(n)
