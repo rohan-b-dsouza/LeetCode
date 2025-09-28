be138be61a04bf30498b784580700eb88ef9ef7e
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-09-28 14:46:31 +0530

    Update final-prices-with-a-special-discount-in-a-shop.java

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.java
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.java
@@ -1,22 +1,26 @@
+// Optimal
+
 class Solution {
     public int[] finalPrices(int[] prices) {
         int n = prices.length;
         int[] ans = new int[n];
         Deque<Integer> stack = new ArrayDeque<>();
         for (int i = n - 1; i >= 0; i--) {
-            // If stack is not empty and stack's peek is greater than or equal to current element, then pop the stack's top
+         // If stack is not empty and stack's top element price is greater than current element then pop it from the stack
             while (!stack.isEmpty() && stack.peek() > prices[i]) {
                 stack.pop();
             }
-            // If stack is empty, then -1 is the NSE of the current element
+             // If stack is empty then no discount is possible hence put current element price as it is to the ans
             if (stack.isEmpty()) ans[i] = prices[i];
-            // Else NSE of the current element is stack's top
+            // Else give discount on current price using stack's top and put this discounted price to the ans
             else ans[i] = prices[i] - stack.peek();
-            // Push current element to the stack
+            // Push current price to the stack
             stack.push(prices[i]);
         }
-        // Return the list
+        // Return the ans
         return ans;
-
     }
 }
+
+// T.C => O(2 * n) = O(n)
+// S.C => O(2 * n) = O(n)
