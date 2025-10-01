73cec0740d76ac0704fbd6185b669f31e9b10c79
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-10-01 18:00:49 +0530

    Update sum-of-subarray-minimums-brute.java

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums-brute.java
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums-brute.java
@@ -8,7 +8,7 @@ class Solution {
         // Generate all subarrays
         for (int i = 0; i < n; i++) {
             int min = Integer.MAX_VALUE;
-          // Track the running minimum and keep adding it to sum
+          // Track the running minimum and keep adding it to the sum
             for (int j = i; j < n; j++) {
                 min = Math.min(min, arr[j]);
                 sum = (sum + min) % MOD;
