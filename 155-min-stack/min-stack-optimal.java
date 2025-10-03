5bab9861118493519a8996882e82a6ea8dba2be9
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-10-03 09:00:21 +0530

    Update and rename min-stack.java to min-stack-optimal.java

---
+++ 155-min-stack/min-stack-optimal.java
@@ -0,0 +1,34 @@
+// Optimal
+
+class MinStack {
+// Stack of int[] to store pairs of {Element, Minimum element seen so far}
+Deque<int[]> stack;
+    // Initialize the stack
+    public MinStack() {
+          stack = new ArrayDeque<>();
+    }
+    public void push(int val) {
+        // If stack is empty, store the pair as {element, element} as only one element seen so far
+        if (stack.isEmpty()) stack.push(new int[] {val, val});
+        // Else 
+        else {
+            // Store the current element with the minimum element seen so far
+            stack.push(new int[] {val, Math.min(val, stack.peek()[1])});
+        }
+    }
+    // Pop the stack's top
+    public void pop() {
+        stack.pop();
+    }
+    // Return the top element
+    public int top() {
+        return stack.peek()[0];
+    }
+    // Return the current minimum element
+    public int getMin() {
+        return stack.peek()[1];
+    }
+}
+
+// T.C => O(1)
+// S.C => O(2 * n) for the stack of int[] => O(n)
