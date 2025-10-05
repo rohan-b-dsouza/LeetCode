aa9b509dc15c0308db5c33c89008d2123969be9e
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-10-05 16:16:09 +0530

    Time: 1843 ms (100.00%) | Memory: 45.7 MB (100.00%) - LeetSync

--- 4019-remove-k-balanced-substrings/remove-k-balanced-substrings.java
+++ 4019-remove-k-balanced-substrings/remove-k-balanced-substrings.java
@@ -1,33 +1,30 @@
 class Solution {
     public String removeSubstring(String s, int k) {
         StringBuilder ans = new StringBuilder();
-        
-        for (char ch : s.toCharArray()) {
-            ans.append(ch);
-            int size = ans.length();
-            
+        int size = 0;
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            ans.append(s.charAt(i));
+            size = ans.length();
             if (size >= 2 * k) {
                 boolean valid = true;
-                
-                // Check if last 2*k characters form '(' * k + ')' * k
-                for (int i = 0; i < k; i++) {
-                    if (ans.charAt(size - 2 * k + i) != '(') {
+                for (int j = 0; j < k; j++) {
+                    if (ans.charAt(size - 2 * k + j) != '(') {
                         valid = false;
                         break;
                     }
-                    if (ans.charAt(size - k + i) != ')') {
+                    if (ans.charAt(size - k + j) != ')') {
                         valid = false;
                         break;
                     }
                 }
-                
-                // If valid, remove last 2*k characters
                 if (valid) {
                     ans.setLength(size - 2 * k);
                 }
+
             }
+
         }
-        
         return ans.toString();
     }
 }
