d45e8d8a5a7b4bb86cdb8251c5d3bbf710634fcd
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-10-08 21:04:09 +0530

    Update online-stock-span-brute.java

--- 937-online-stock-span/online-stock-span-brute.java
+++ 937-online-stock-span/online-stock-span-brute.java
@@ -1,9 +1,25 @@
 // Brute
 
 
-class Solution {
-    public int[] stockSpan(int[] arr, int n) {
-        int[] ans = new int[n];
+class StockSpanner {
+    private Deque<int[]> stack; // pair = {price, span}
+    // Initialize the stack
+    public StockSpanner() {
+        stack = new ArrayDeque<>();
+    }
+    public int next(int price) {
+         // Initialize span to 1
+        int span = 1;
+        
+        // Push the current price to the stack
+        stack.push(new int[]{price, span});
+        // Return span
+        return span;
+    }
+}
+
+
+int[] ans = new int[n];
         for (int i = 0; i < n; i++) {
             int span = 1;
             for (int j = i - 1; j >= 0; j--) {
@@ -13,6 +29,3 @@ class Solution {
             ans[i] = span;
         }
         return ans;
-    }
-}
-
