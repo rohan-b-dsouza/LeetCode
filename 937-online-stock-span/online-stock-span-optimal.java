2cc3a02be5da9e6d8c623799ab860cdc8353e76c
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-10-08 20:52:32 +0530

    Update and rename online-stock-span.java to online-stock-span-optimal.java

---
+++ 937-online-stock-span/online-stock-span-optimal.java
@@ -0,0 +1,31 @@
+// Optimal (Refer GFG version of this problem to understand better as the leetcode one is poorly described and also unnecessary twist of design has been made which makes it confusing)
+
+class StockSpanner {
+    private Deque<int[]> stack; // pair = {price, span}
+    // Initialize the stack
+    public StockSpanner() {
+        stack = new ArrayDeque<>();
+    }
+    public int next(int price) {
+         // Initialize span to 1
+        int span = 1;
+        // If stack is not empty, and current stock price is greater than peek stock price, add peek's span to span
+        while (!stack.isEmpty() && price >= stack.peek()[1) {
+            span += stack.pop()[1];
+        }
+        // Push the current price to the stack
+        stack.push(new int[]{price, span});
+        // Return span
+        return span;
+    }
+}
+
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
+
+// T.C => O(n)
+// S.C => O(n)
