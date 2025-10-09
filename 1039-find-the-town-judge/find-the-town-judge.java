89d03b23c3fb113455c7f1ac21e9e484b14c531b
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-10-09 21:04:23 +0530

    Time: 3 ms (66.23%) | Memory: 53.8 MB (74.80%) - LeetSync

---
+++ 1039-find-the-town-judge/find-the-town-judge.java
@@ -0,0 +1,29 @@
+// Brute
+
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+
+        // Array that tracks how many people trust a person
+        int[] trustsMe = new int[n + 1];
+        // Array that tracks how many people a person trusts
+        int[] Itrust = new int[n + 1];
+        // Iterate over the entire array and set the above two arrays
+        for (int[] t : trust) {
+            int a = t[0];
+            int b = t[1];
+            Itrust[a]++;
+            trustsMe[b]++;
+        }
+        for (int j = 1; j <= n; j++) {
+            // For each person check if its trusted by all other people (i.e n - 1) and also check if it doesn't trust anyone else. If so then this is the celebrity
+            if (trustsMe[j] == n - 1 && Itrust[j] == 0) {
+                return j;
+            }
+        }
+        return -1;
+    }
+
+}
+
+// T.C => O(n ^ 2) + O(n) = O(n ^ 2)
+// S.C => O(2 * n) = O(n)
