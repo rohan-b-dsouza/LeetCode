148129cb0eb0aaf76f767e4c454e1a94687a00e0
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-10-09 21:23:07 +0530

    Update find-the-town-judge-brute.java

--- 1039-find-the-town-judge/find-the-town-judge-brute.java
+++ 1039-find-the-town-judge/find-the-town-judge-brute.java
@@ -1,4 +1,5 @@
-// Brute (VVIMP : Refer the GFG problem 'The Celebrity Problem' too, its same with a difference in how the input is given)
+// Brute 
+// VVIMP : Refer the GFG Problem 'The Celebrity Problem' in which the input is given in a different way but it changes the solutions significantly, so its very important to solve that problem too.
 
 class Solution {
     public int findJudge(int n, int[][] trust) {
