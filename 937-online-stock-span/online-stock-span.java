7c75540059771461ee714a2b99029a15bba568c8
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-10-14 07:34:10 +0530

    Time: 24 ms (95.87%) | Memory: 55.2 MB (80.78%) - LeetSync

---
+++ 937-online-stock-span/online-stock-span.java
@@ -0,0 +1,23 @@
+class StockSpanner {
+    private Deque<int[]> stack; // pair = {price, span}
+
+    public StockSpanner() {
+        stack = new ArrayDeque<>();
+    }
+
+    public int next(int price) {
+        int span = 1;
+        while (!stack.isEmpty() && price >= stack.peek()[0]) {
+            span += stack.pop()[1];
+        }
+        stack.push(new int[]{price, span});
+        return span;
+    }
+}
+
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
