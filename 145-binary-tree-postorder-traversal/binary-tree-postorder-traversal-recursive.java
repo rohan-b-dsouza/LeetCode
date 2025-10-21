9e7218e440b31a66c196263da940255313c893d2
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-10-21 18:42:34 +0530

    Update and rename binary-tree-postorder-traversal.java to binary-tree-postorder-traversal-recursive.java

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal-recursive.java
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal-recursive.java
@@ -1,3 +1,5 @@
+// Recursive
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -13,17 +15,27 @@
  *     }
  * }
  */
+
 class Solution {
     public void recursivePostorder(TreeNode node, List<Integer> ans) {
-if (node == null) return;
+    // If node is null, return    
+    if (node == null) return;
+        // Recursively traverse left
         recursivePostorder(node.left, ans);
+        // Recursively traverse right
         recursivePostorder(node.right, ans);
+        // Add current node to 'ans'
         ans.add(node.val);
     }
 
     public List<Integer> postorderTraversal(TreeNode root) {
+        // List to store the answer
         List<Integer> ans = new ArrayList<>();
+        // Call the recursive function
         recursivePostorder(root, ans);
         return ans;
     }
 }
+
+// T.C => O(n) as we visit all the nodes
+// S.C => O(n) for skewed B.T and O(log (n)) for balanced B.T
