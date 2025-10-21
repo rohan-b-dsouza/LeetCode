3c551d244fddc2d063352609fbc661b955e7888e
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-10-21 18:47:47 +0530

    Update binary-tree-postorder-traversal.java

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -1,3 +1,5 @@
+// Iterative
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -13,19 +15,34 @@
  *     }
  * }
  */
+
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
+        // If tree is empty, return empty list
         if (root == null) return new ArrayList<>();
+        // List to store the answer
         List<Integer> ans = new ArrayList<>();
+        // Stack to backtrack to previous nodes
         Deque<TreeNode> st = new ArrayDeque<>();
+        // Push root node to the stack
         st.push(root);
+        // Logic for Node - Right - Left, Reversing final output gives Left - Right - Node
         while(!st.isEmpty()) {
+            // Get the stack's top node 
             root = st.pop();
+            // Add it to the answer
             ans.add(root.val);
+            // If left node exists push it to the stack
             if (root.left != null) st.push(root.left);
+            // If right node exists push it to the stack
             if (root.right != null) st.push(root.right);
         }
+        // Reverse the ans to get the Postorder Traversal
         Collections.reverse(ans);
+        // Return the answer
         return ans;
     }
 }
+
+// T.C => O(2 * n) = O(n)
+// S.C => O(n) for skewed B.T and O(log (n)) for balanced B.T
