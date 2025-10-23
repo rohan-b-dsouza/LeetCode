6ddf8cff603ae4482c32be3f1009f8de5f4b8c62
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-10-23 15:55:18 +0530

    Update and rename same-tree.java to same-tree-recursive.java

--- 100-same-tree/same-tree-recursive.java
+++ 100-same-tree/same-tree-recursive.java
@@ -1,3 +1,5 @@
+// Recursive (Preorder Traversal)
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -15,8 +17,14 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
+        // If either of the two nodes at any step is null then check if whether both are null or not. If both are null then return true but if one of them is null then they are not identical hence return false
         if (p == null || q == null) return p == q;
+        // If nodes are not equal, return false
         if (p.val != q.val) return false;
+        // Preorder traversal
         return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
+
+// T.C => O(n) as we visit all nodes in the worst case i.e both trees are identical
+// S.C => O(n) in worst case (skewed B.T) or O(log (n)) for balanced tree 
