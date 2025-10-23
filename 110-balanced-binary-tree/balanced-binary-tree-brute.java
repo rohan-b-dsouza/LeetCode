bd07f580444f660b190172514e26368197ecb957
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-10-23 19:21:54 +0530

    Update and rename balanced-binary-tree.java to balanced-binary-tree-brute.java

--- 110-balanced-binary-tree/balanced-binary-tree-brute.java
+++ 110-balanced-binary-tree/balanced-binary-tree-brute.java
@@ -1,3 +1,5 @@
+// Brute (Preorder)
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -16,16 +18,23 @@
 
 
 class Solution {
+	// Get the height of this subtree using maxDepth function
     public int maxDepth(TreeNode node) {
         if (node == null) return 0;
         return 1 + Math.max(maxDepth(node.left), maxDepth(node.right));
         
     }
     public boolean isBalanced(TreeNode root) {
+		// If root is null, return true
         if (root == null) return true;
+		// Check if tree is balanced at this node
         if (Math.abs(maxDepth(root.left) - maxDepth(root.right)) > 1) {
             return false;
         }
+		// Recursively check all nodes first left nodes then right nodes
         return isBalanced(root.left) && isBalanced(root.right);
 	}
 }
+
+// T.C => O(2 * (n ^ 2)) = O(n ^ 2) as there are total n nodes and for each node we calculate height of left as well as right subtree
+// S.C => O(2 * n) = O(n) as there are 2 recursive stacks, considering worst case i.e Skewed B.T , Best Case : O(2 * log (n)) = O(log (n))
