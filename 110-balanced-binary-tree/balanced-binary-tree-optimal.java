8c062ab718208254cf42182027002deca17c1d9c
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-10-23 19:38:37 +0530

    Update and rename balanced-binary-tree.java to balanced-binary-tree-optimal.java

--- 110-balanced-binary-tree/balanced-binary-tree-optimal.java
+++ 110-balanced-binary-tree/balanced-binary-tree-optimal.java
@@ -1,3 +1,5 @@
+// Optimal (Postorder)
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -17,15 +19,26 @@
 
 class Solution {
     public int maxDepth(TreeNode node) {
+		// If node is null, return 0
         if (node == null) return 0;
+		// Recursively get the height of the left subtree
         int left = maxDepth(node.left);
+		// If left is -1 at any point, it means tree is not balanced hence return -1
         if (left == -1) return -1;
+		// Recursively get the height of the right subtree
         int right = maxDepth(node.right);
-        if (right == -1) return -1;
+        // If left is -1 at any point, it means tree is not balanced hence return -1
+		if (right == -1) return -1;
+		// Check if tree is balanced, if not return -1
         if (Math.abs(left - right) > 1) return -1;
+		// Return the height of current subtree
         return 1 + Math.max(left, right);
     }
     public boolean isBalanced(TreeNode root) {
+		// If the helper function maxDepth returns -1, it means tree is not balanced else it's balanced
         return maxDepth(root) != -1;
 	}
 }
+
+// T.C => O(n) as we visit each node once
+// S.C => O(n) in worst case for a skewed B.T and O(log (n)) in best case for a balanced B.T
