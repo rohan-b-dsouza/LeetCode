6776d9f02772220a350634b96a623f68cc13da97
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-10-24 15:43:44 +0530

    Update diameter-of-binary-tree-brute.java

--- 543-diameter-of-binary-tree/diameter-of-binary-tree-brute.java
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree-brute.java
@@ -1,3 +1,5 @@
+// Brute
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -14,15 +16,23 @@
  * }
  */
 class Solution {
+    // Function to get the height of the subtrees
     public int maxDepth(TreeNode node) {
         if (node == null) return 0;
         return 1 + Math.max(maxDepth(node.left), maxDepth(node.right));
     }
     public int diameterOfBinaryTree(TreeNode root) {
+         // If root is null return 0
         if (root == null) return 0;
+        // Get the left and right subtree height for current node
         int leftHeight = maxDepth(root.left);
         int rightHeight = maxDepth(root.right);
+        // Compute the path length taking current node as pivot)
         int diameter = leftHeight + rightHeight;
+        // Recursively compute the path lengths by taking each node as pivot and return the maximum path length (diameter of the B.T)
         return Math.max(Math.max(diameterOfBinaryTree(root.left), diameter), diameterOfBinaryTree(root.right));
     }
 }
+
+// T.C => O(n ^ 2)
+// S.C => O(2 * n) = O(n) as two recursion stacks will be reqd one for maxDepth func calls and one for diameterOfBinaryTree() func calls
