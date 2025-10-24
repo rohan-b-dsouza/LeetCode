232e65bb17e8cf0d0a21713bac5fd23c67da0108
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-10-24 11:48:44 +0530

    Time: 2 ms (100.00%) | Memory: 55.7 MB (67.89%) - LeetSync

--- 3967-earliest-finish-time-for-land-and-water-rides-ii/earliest-finish-time-for-land-and-water-rides-ii.java
+++ 3967-earliest-finish-time-for-land-and-water-rides-ii/earliest-finish-time-for-land-and-water-rides-ii.java
@@ -1,35 +1,38 @@
+// Optimal (Greedy Approach)
+
 class Solution {
     public int earliestFinishTime(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration) {
         int l1 = landStartTime.length;
         int w1 = waterStartTime.length;
         int minTime = Integer.MAX_VALUE;
+        
+        // Case 1 : First Land, Then Water
         int minLand = Integer.MAX_VALUE;
+        // Get the land ride with minimum reqd completion time
         for (int i = 0; i < l1; i++) {
             minLand = Math.min(minLand, landStartTime[i] + landDuration[i]);
         }
-        for (int j = 0; j < w1; j++) {
-            
-            if (minLand < waterStartTime[j]) {
-                minTime = Math.min(minTime, waterStartTime[j] + waterDuration[j]);
-            }
-            else {
-                minTime = Math.min(minTime, minLand + waterDuration[j]);
-            }
-        }
+
         int minWater = Integer.MAX_VALUE;
-        for (int i = 0; i < w1; i++) {
-            minWater = Math.min(minWater, waterStartTime[i] + waterDuration[i]);
+        // Find the water ride which adds least possible time to this time
+        for (int j = 0; j < w1; j++) { 
+            minTime = Math.min(minTime, Math.max(waterStartTime[j], minLand) + waterDuration[j]);
+            // Get the water ride with minimum reqd completion time in this loop itself instead of using separate loop for this
+            minWater = Math.min(minWater, waterStartTime[j] + waterDuration[j]);
         }
+
+        // Case 2 : First Water, then Land
+        // We have computed the water ride with minimum reqd completion time in previous loop itself
+        // Now, Find the land ride which adds least possible time to the this time
+
         for (int j = 0; j < l1; j++) {
-            int sum = 0;
-            if (minWater < landStartTime[j]) {
-                minTime = Math.min(minTime, landStartTime[j] + landDuration[j]);
-            }
-            else {
-                minTime = Math.min(minTime, minWater + landDuration[j]);
-            }
+            minTime = Math.min(minTime, Math.max(landStartTime[j], minWater) + landDuration[j]);
         }
+
+        // Return minTime;
         return minTime;
-        
     }
 }
+
+// T.C => O(2 * (n + m)) = O(n + m)
+// S.C => O(1)
