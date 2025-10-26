0796c077f90f8caaa93d460fa4ff28a79d27158b
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-10-26 13:50:38 +0530

    Time: 102 ms (91.54%) | Memory: 45.7 MB (0.00%) - LeetSync

---
+++ 4073-lexicographically-smallest-string-after-reverse/lexicographically-smallest-string-after-reverse.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String lexSmallest(String s) {
+        int n = s.length();
+        StringBuilder ans = new StringBuilder("");
+        for (int i = 1; i <= n; i++) {
+            StringBuilder firstK = new StringBuilder(s.substring(0, i)).reverse().append(s.substring(i));
+            StringBuilder lastK = new StringBuilder();
+            lastK.append(s.substring(0, n - i));
+            lastK.append(new StringBuilder(s.substring(n - i)).reverse());
+            if ((ans.length() == 0) || (firstK.compareTo(ans) < 0)) ans = firstK;
+            if (lastK.compareTo(ans) < 0) ans = lastK;
+        }
+        return ans.toString();
+    }
+}
