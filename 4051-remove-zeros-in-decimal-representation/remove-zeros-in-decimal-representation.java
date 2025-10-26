d253c25e70a599346470f938262ff525ba42ef32
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-10-26 16:38:03 +0530

    Time: 1 ms (100.00%) | Memory: 41.1 MB (0.00%) - LeetSync

--- 4051-remove-zeros-in-decimal-representation/remove-zeros-in-decimal-representation.java
+++ 4051-remove-zeros-in-decimal-representation/remove-zeros-in-decimal-representation.java
@@ -1,29 +1,21 @@
 class Solution {
-    public long getPower(long base, long expo) {
-        long ans = 1;
-        while (expo > 0) {
-            if (expo % 2 == 0) {
-                expo /= 2;
-                base = base * base;
-            }
-            else {
-                expo--;
-                ans = ans * base;
-            }
-        }
-        return ans;
-    }
     public long removeZeros(long n) {
         long ans = 0;
         long cnt = 0;
         while (n > 0) {
             long digit = n % 10;
             if (digit != 0) {
-            ans = (digit * getPower(10, cnt) + ans);
+            ans = ((ans * 10) + digit);
             cnt++;
             }
             n = n / 10;  
         }
-        return ans;
+        long finalAns = 0;
+        while (ans > 0) {
+            long digit = ans % 10;
+            finalAns = (finalAns * 10 + digit);
+            ans = ans / 10;
+        }
+        return finalAns;
     }
 }
