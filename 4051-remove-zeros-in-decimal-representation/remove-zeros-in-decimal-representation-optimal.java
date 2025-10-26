f337b27a0936c4fa3b72e73f77d033078bff15c8
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-10-26 16:42:16 +0530

    Update and rename remove-zeros-in-decimal-representation.java to remove-zeros-in-decimal-representation-optimal.java

--- 4051-remove-zeros-in-decimal-representation/remove-zeros-in-decimal-representation-optimal.java
+++ 4051-remove-zeros-in-decimal-representation/remove-zeros-in-decimal-representation-optimal.java
@@ -1,21 +1,29 @@
+// Optimal 
+
 class Solution {
     public long removeZeros(long n) {
         long ans = 0;
-        long cnt = 0;
+        // First get all the non-zero digits into ans
+        // This will give us the reversed result as digits will be extracted from last 
         while (n > 0) {
             long digit = n % 10;
             if (digit != 0) {
             ans = ((ans * 10) + digit);
-            cnt++;
             }
             n = n / 10;  
         }
+        // Stores the final answer
         long finalAns = 0;
+        // Now, reverse the ans to get the required final answer
         while (ans > 0) {
             long digit = ans % 10;
             finalAns = (finalAns * 10 + digit);
             ans = ans / 10;
         }
+        // Return finalAns
         return finalAns;
     }
 }
+
+// T.C => O(log (n))
+// S.C => O(1)
