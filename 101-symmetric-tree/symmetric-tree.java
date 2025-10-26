ad4fe67c7759e10f7b67f47abf07f534df43de98
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-10-26 22:53:22 +0530

    Update symmetric-tree.java

--- 101-symmetric-tree/symmetric-tree.java
+++ 101-symmetric-tree/symmetric-tree.java
@@ -1,3 +1,5 @@
+// Optimal (Preorder)
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -15,14 +17,22 @@
  */
 class Solution {
     public boolean checkSymmetry(TreeNode leftTree, TreeNode rightTree) {
+        // If both left tree and right tree mirror nodes are null, return true else false
         if (leftTree == null || rightTree == null)
             return leftTree == rightTree;
+        // If both left tree and right tree have same data, return true else false
         if (leftTree.val != rightTree.val)
             return false;
+        // Do NLR on left tree and NRL on right tree simultaneously
         return checkSymmetry(leftTree.left, rightTree.right) && checkSymmetry(leftTree.right, rightTree.left);
     }
-
+    
     public boolean isSymmetric(TreeNode root) {
+        // If tree is empty, return true
+        // Else check whether tree is symmetric or not
         return ((root == null) || checkSymmetry(root.left, root.right));
     }
 }
+
+// T.C => O(n) since we visit all the nodes once
+// S.C => O(n) for skewed B.T and O(log (n)) for balanced B.T
