619d244bc5ba1ebad19c825120b394930cb47828
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-10-29 19:32:44 +0530

    Update and rename binary-tree-right-side-view.java to binary-tree-right-side-view-optimal-2-dfs.java

--- 199-binary-tree-right-side-view/binary-tree-right-side-view-optimal-2-dfs.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view-optimal-2-dfs.java
@@ -1,3 +1,6 @@
+// Optimal
+// DFS (Preorder)
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -15,16 +18,26 @@
  */
 class Solution {
     public void rightSideView(TreeNode node, int level, List<Integer> ans) {
+        // If node is null, return
         if (node == null) return;
+        // If size of ans list is equal to level it means we haven't got the node for this level till now hence take the current node
         if (ans.size() == level) {
             ans.add(node.val);
         }
+        // Recursively traverse in the right subtree with incremented level
         rightSideView(node.right, level + 1, ans);
+        // Recursively traverse in the left subtree with incremented level
         rightSideView(node.left, level + 1, ans);
     }
     public List<Integer> rightSideView(TreeNode root) {
+        // List to store the ans
         List<Integer> ans = new ArrayList<>();
+        // Call the recursive function
         rightSideView(root, 0, ans);
+        // Return ans
         return ans;
     }
 }
+
+// T.C => O(N) as we visit each node once
+// S.C => O(N) for a skewed tree and O(log (N)) for a balanced tree
