218948ebc18a7cfbdabec0a6b49292ce4517212a
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-10-29 19:36:26 +0530

    Update binary-tree-right-side-view-optimal-1-bfs.java

--- 199-binary-tree-right-side-view/binary-tree-right-side-view-optimal-1-bfs.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view-optimal-1-bfs.java
@@ -3,12 +3,18 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int data;
+ *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int val) { data = val; left = null, right = null }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
- **/
+ */
 
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
