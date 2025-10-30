d9cc475d5bf91cca41a624dd58e7a465d4119e05
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-10-30 15:45:01 +0530

    Time: 1 ms (98.25%) | Memory: 42.5 MB (86.31%) - LeetSync

--- 257-binary-tree-paths/binary-tree-paths.java
+++ 257-binary-tree-paths/binary-tree-paths.java
@@ -15,25 +15,34 @@
  */
 class Solution {
    public void getAllPaths(TreeNode node, List<String> ans, StringBuilder path) {
-        int len = path.length(); // store current length for backtracking
+        int len = path.length(); // stores current length for backtracking
+        // Append current node value to the path
         path.append(node.val);
 
         // If it's a leaf node, add the current path to the answer list
         if (node.left == null && node.right == null) {
             ans.add(path.toString());
-        } else {
+        } 
+        // Else, 
+        else {
+            // Append -> to the path
             path.append("->");
+            // If left node is present move to the left node
             if (node.left != null) getAllPaths(node.left, ans, path);
+            // If right node is present move to the right node
             if (node.right != null) getAllPaths(node.right, ans, path);
         }
-
         path.setLength(len); // backtrack to previous state
     }
 
     public List<String> binaryTreePaths(TreeNode root) {
+        // List to store the answer
         List<String> ans = new ArrayList<>();
+        // If tree is empty, return empty list
         if (root == null) return ans;
+        // Call the recursive function
         getAllPaths(root, ans, new StringBuilder());
+        // Return ans
         return ans;
     }
 }
