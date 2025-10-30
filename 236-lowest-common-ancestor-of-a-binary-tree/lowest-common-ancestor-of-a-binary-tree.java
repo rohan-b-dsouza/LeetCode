2fe09e6714a9da4b6b2dc6b372b3d725b8c27a5b
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-10-30 22:24:48 +0530

    Update lowest-common-ancestor-of-a-binary-tree.java

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -1,3 +1,5 @@
+// Brute
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -9,23 +11,36 @@
  */
 class Solution {
     public boolean rootToNodePath(TreeNode node, TreeNode target, List<TreeNode> path) {
-         if (node == null) return false;
+        // If reached null, return false 
+        if (node == null) return false;
+        // Add current node to the path
         path.add(node);
+        // If node is equal to target return true as path is obtained
         if (node == target) return true;
+        // If any path returns true return true
         if (rootToNodePath(node.left, target, path) || rootToNodePath(node.right, target, path))
             return true;
+        // Backtrack by removing the last node
         path.remove(path.size() - 1);
+        // Return false if path is not obtained
         return false;
     }
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        // Lists to store the paths
         List<TreeNode> path1 = new ArrayList<>();
         List<TreeNode> path2 = new ArrayList<>();
+        // Get the paths for p and q
     rootToNodePath(root, p, path1);
         rootToNodePath(root, q, path2);
         int i = 0;
+        // Traverse over both the paths till both have same nodes, this helps in taking the lowest comment ancestor
         while (i < path1.size() && i < path2.size() && path1.get(i) == path2.get(i)) {
             i++;
         }
+        // Return lca
         return path1.get(i - 1);
     }
 }
+
+// T.C => O(2 * N) = O(N) in the worst case as we have to find both the paths
+// S.C => O(2 * N) = O(N) for the recursive stacks for path1 and path2
