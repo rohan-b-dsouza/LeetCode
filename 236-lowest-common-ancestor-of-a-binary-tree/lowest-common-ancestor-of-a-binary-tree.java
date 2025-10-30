48a5a47b56392daa912b0714b596f4e14e271c22
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-10-30 19:00:44 +0530

    Update lowest-common-ancestor-of-a-binary-tree.java

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -1,3 +1,5 @@
+// Optimal (DFS - Preorder Traversal)
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -9,15 +11,24 @@
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-         if (root == null || root == p || root == q) return root;
+        // If root is null or root matches with either p or q, then return root
+        if (root == null || root == p || root == q) return root;
+        // Recursively traverse the left subtree
         TreeNode left = lowestCommonAncestor(root.left, p, q);
+        // Recursively traverse the right subtree
         TreeNode right = lowestCommonAncestor(root.right, p, q);
+        // If left is null, then return right
         if (left == null) {
             return right;
         }
+        // If right is null, then return left
         else if (right == null) {
             return left;
         }
+        // If both are null, it means the current node is the lca hence return it
         else return root;
     }
 }
+
+// T.C => O(N) as we visit all the nodes
+// S.C => O(N) for skewed B.T and O(log (n)) for balanced B.T
