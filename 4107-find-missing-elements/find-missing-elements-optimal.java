3c3031e9fddb7f52877d2eb4fc4e514bc4cdbd35
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-11-02 12:21:00 +0530

    Update and rename find-missing-elements.java to find-missing-elements-optimal.java

--- 4107-find-missing-elements/find-missing-elements-optimal.java
+++ 4107-find-missing-elements/find-missing-elements-optimal.java
@@ -1,19 +1,30 @@
+// Optimal
+
 class Solution {
     public List<Integer> findMissingElements(int[] nums) {
+        // List to store the answer
         List<Integer> ans = new ArrayList<>();
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
+        // HashSet for fast lookups
         HashSet<Integer> set = new HashSet<>();
+        // Get the min and max elements of the array, also add all the elements to the HashSet
         for (int num : nums) {
             min = Math.min(min, num);
             max = Math.max(max, num);
             set.add(num);
         }
+        // Iterate from min to max as elements are in the range [min, max]
+        // Check if element is present in the set, if not then add it to the answer list
         for (int i = min; i <= max; i++) {
             if (!set.contains(i)) {
                 ans.add(i);
             }
         }
+        // Return ans
         return ans;
     }
 }
+
+// T.C => O(n) + O(max - min + 1) = O(max - min + 1)
+// S.C => O(n) for the HashSet
