8d5db81a6d20ca99dd24074003d3ddb5a0072768
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-11-02 23:32:13 +0530

    Update all-nodes-distance-k-in-binary-tree-optimal.java

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree-optimal.java
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree-optimal.java
@@ -1,9 +1,21 @@
 // Optimal
 
 /**
- * Definition for a binary tree node. public class TreeNode { int data; TreeNode left; TreeNode
- * right; TreeNode(int val) { data = val; left = null, right = null } }
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
  */
+
 class Solution {
   public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
     // List to store the answer
