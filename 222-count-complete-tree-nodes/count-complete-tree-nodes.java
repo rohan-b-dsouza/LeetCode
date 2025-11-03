d597ed13802253209c2bd70d95b4caed5721c36b
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-11-03 22:20:03 +0530

    Update count-complete-tree-nodes.java

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.java
@@ -1,3 +1,5 @@
+// Optimal 
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -14,6 +16,7 @@
  * }
  */
 class Solution {
+    // Function to get height of left subtree
     public int getLeftHeight(TreeNode root) {
         int height = 0;
         while (root != null) {
@@ -22,6 +25,7 @@ class Solution {
         }
         return height;
     }
+    // Function to get height of right subtree
     public int getRightHeight(TreeNode root) {
         int height = 0;
         while (root != null) {
@@ -30,12 +34,21 @@ class Solution {
         }
         return height;
     }
-
     public int countNodes(TreeNode root) {
+        // If root is null return 0
         if (root == null) return 0;
+        // Get height of left subtree
         int left = getLeftHeight(root);
+        // Get height of right subtree
         int right = getRightHeight(root);
+        // If left subtree and right subtree are equal in height, it means it's a complete subtree hence we can directly compute the no. of nodes in this subtree using the formula 2^h - 1 where h = height of the subtree
         if (left == right) return ((1 << left) - 1);
+        // If left is not equal to right, then count the current node and move to left node and then right node, recursively accumulating the count of nodes
         return 1 + countNodes(root.left) + countNodes(root.right);
     }
 }
+
+// T.C => O(log(n) * (log(n)) = O((log(n))^2)
+
+
+
