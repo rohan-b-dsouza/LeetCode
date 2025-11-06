38fe604b837cd7b10f905884777ec69f7dd70ed5
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-11-06 22:23:32 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (5.31%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -13,40 +13,33 @@
  *     }
  * }
  */
+
+
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-        // List to store inorder traversal
-        List<Integer> inorder = new ArrayList<>(); 
-        // Pointer to the current node
-        TreeNode cur = root; 
-
-        while (cur != null) {
-            if (cur.left == null) {
-                // Add current node's value and move to right child
-                inorder.add(cur.val); 
-                cur = cur.right; 
-            } else {
-                // Find the predecessor
-                TreeNode prev = cur.left; 
-                while (prev.right != null && prev.right != cur) {
-                    prev = prev.right;
+        List<Integer> ans = new ArrayList<>();
+        TreeNode curr = root;
+        while (curr != null) {
+            if (curr.left == null) {
+                ans.add(curr.val);
+                curr = curr.right;
+            }
+            else {
+                TreeNode rightMostNode = curr.left;
+                while(rightMostNode.right != null && rightMostNode.right != curr) {
+                    rightMostNode = rightMostNode.right;
                 }
-
-                /* Establish a temporary link and move to the
-                left child */
-                if (prev.right == null) {
-                    prev.right = cur; 
-                    cur = cur.left; 
-                } else {
-                /* Remove the temporary link, add current node's value
-                and move to the right child */    
-                    prev.right = null; 
-                    inorder.add(cur.val); 
-                    cur = cur.right; 
+                if (rightMostNode.right == null) {
+                    rightMostNode.right = curr;
+                    curr = curr.left;
+                }
+                else {
+                    rightMostNode.right = null;
+                    ans.add(curr.val);
+                    curr = curr.right;
                 }
             }
         }
-        // Return inorder traversal
-        return inorder; 
+        return ans;
     }
 }
