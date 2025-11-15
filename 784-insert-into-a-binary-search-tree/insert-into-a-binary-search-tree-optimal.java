912656bdbe629f7689a0f27241b8137560da63d7
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-11-15 20:38:26 +0530

    Update and rename insert-into-a-binary-search-tree.java to insert-into-a-binary-search-tree-optimal.java

--- 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree-optimal.java
+++ 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree-optimal.java
@@ -1,3 +1,5 @@
+// Optimal
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -16,12 +18,19 @@
 
 class Solution {
     public TreeNode insertNode(TreeNode node, int val) {
+        // If node is null, create a node with the target value and return it
         if (node == null) return new TreeNode(val);
+        // If val is smaller than current node's val recursively move left
         if (val < node.val) node.left = insertNode(node.left, val);
+        // Else move right
         else if (val > node.val) node.right = insertNode(node.right, val);
+        // Return node
         return node;
     }
     public TreeNode insertIntoBST(TreeNode root, int val) {
         return insertNode(root, val);
     }
 }
+
+// T.C => O(h) ---------- {h = log (n) for a balanced BST, h = n for a skewed BST}
+// S.C => O(h) ---------- {h = log (n) for a balanced BST, h = n for a skewed BST}
