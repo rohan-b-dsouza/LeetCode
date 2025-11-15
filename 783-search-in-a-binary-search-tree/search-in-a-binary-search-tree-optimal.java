a198226aca7b9b9d762b09add52df9429c19d9a4
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-11-15 18:38:31 +0530

    Update and rename search-in-a-binary-search-tree.java to search-in-a-binary-search-tree-optimal.java

--- 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree-optimal.java
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree-optimal.java
@@ -1,3 +1,5 @@
+// Optimal
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -15,10 +17,17 @@
  */
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
+        // Get the root node
         TreeNode node = root;
+        // If node is not null and node's val is not equal to target value
         while (node != null && node.val != val) {
+            // If target is smaller than current node's val move left else move right
             node = (val < node.val) ? node.left : node.right;
         }
+        // Return node
         return node;
     }
 }
+
+// T.C => O(log (n))
+// S.C => O(1)
