502fa0d344cab57086e039d36fb8bc618a5d8d33
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-11-16 13:07:11 +0530

    Update and rename minimum-string-length-after-balanced-removals.java to minimum-string-length-after-balanced-removals-optimal.java

--- 4090-minimum-string-length-after-balanced-removals/minimum-string-length-after-balanced-removals-optimal.java
+++ 4090-minimum-string-length-after-balanced-removals/minimum-string-length-after-balanced-removals-optimal.java
@@ -1,11 +1,18 @@
+// Optimal
+
 class Solution {
     public int minLengthAfterRemovals(String s) {
         int cnta = 0;
         int cntb = 0;
+        // Get the count of 'a's and 'b's in the string
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == 'a') cnta++;
             else cntb++;
         }
+        // The minimum possible length of the string after removals is abs(a - b)
         return (Math.abs(cnta - cntb));
     }
 }
+
+// T.C => O(n)
+// S.C => O(1)
