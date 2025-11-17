b5d62746444950115fd65d51350037f55073c332
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-11-17 19:18:01 +0530

    Update kth-smallest-element-in-a-bst-optimal.java

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst-optimal.java
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst-optimal.java
@@ -1,3 +1,5 @@
+// Semi - Optimal
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -15,19 +17,34 @@
  */
 class Solution {
     public void inorder(TreeNode root, int k,int[] ans, int[] cnt) {
-    if (root == null) return;
+    // If root is null or we have got the answer, return
+    if (root == null || cnt[0] == -1) return;
+    // Recursively move left
     inorder(root.left, k, ans, cnt);
+    // Increment counter
     cnt[0]++;
+    // If counter equals k it means we are at the desired node
     if (cnt[0] == k) {
+      // Store the value in ans
       ans[0] = root.val;
+      // Set counter to -1 to mark that we have got the answer to ensure early exit from the recursion
+      cnt[0] = -1;
+      // Return;
       return;
     }
+    // Recursively move right
     inorder(root.right, k, ans, cnt);
   }
     public int kthSmallest(TreeNode root, int k) {
+        // Arrays to store the counter and ans
         int[] cnt = new int[1];
         int[] ans = new int[1];
+        // Call the recursive function
         inorder(root, k, ans, cnt);
+        // Return ans
         return ans[0];
     }
 }
+
+// T.C => O(n) as we visit all nodes in the worst case 
+// S.C => O(h) for the recursive stack --------- {h = log (n) for balanced BST, h = n for skewed BST}
