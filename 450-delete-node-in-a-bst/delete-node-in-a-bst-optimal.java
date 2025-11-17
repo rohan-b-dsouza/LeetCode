c52aa96f5229ef6f98c2f05eef3f367c62a66dcd
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-11-17 14:49:05 +0530

    Update delete-node-in-a-bst-optimal.java

--- 450-delete-node-in-a-bst/delete-node-in-a-bst-optimal.java
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst-optimal.java
@@ -1,12 +1,4 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int data;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int val) { data = val; left = null, right = null }
- * }
- **/
+// Optimal
 
 class Solution {
     public TreeNode deleteNode(TreeNode root, int key) {
