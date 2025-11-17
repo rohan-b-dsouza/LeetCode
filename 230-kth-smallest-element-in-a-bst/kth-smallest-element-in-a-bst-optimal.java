f63137d2b8e3ad8f511dbcd3b3b49aa9589c3415
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-11-17 20:45:31 +0530

    Update kth-smallest-element-in-a-bst-optimal.java

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst-optimal.java
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst-optimal.java
@@ -1,4 +1,5 @@
 // Optimal (We simply use the fact that inorder traversal of a BST is in sorted order)
+// For the problem Kth Greatest element in a bst, refer end of this page
 
 /**
  * Definition for a binary tree node.
@@ -54,3 +55,54 @@ class Solution {
 
 // T.C => O(n) 
 // S.C => O(1)
+
+// Kth greatest element in a bst
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int data;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int val) { data = val; left = null, right = null }
+ * }
+ **/
+
+class Solution {
+    public int getInorder(TreeNode root, int k, int[] cnt) {
+        cnt[0] = 0;
+        TreeNode curr = root;
+        int ans = -1;
+        while (curr != null) {
+            if (curr.left == null) {
+                cnt[0]++;
+                if (cnt[0] == k) ans = curr.data;
+                curr = curr.right;
+            }
+            else {
+                TreeNode rightMostNode = curr.left;
+                while(rightMostNode.right != null && rightMostNode.right != curr) {
+                    rightMostNode = rightMostNode.right;
+                }
+                if (rightMostNode.right == null) {
+                    rightMostNode.right = curr;
+                    curr = curr.left;
+                }
+                else {
+                    rightMostNode.right = null;
+                    cnt[0]++;
+                    if (cnt[0] == k) ans = curr.data;
+                    curr = curr.right;
+                }
+            }
+        }
+        return ans;
+    }
+    public List<Integer> kLargesSmall(TreeNode root, int k) {
+       List<Integer> ans = new ArrayList<>();
+       int cnt[] = new int[1];
+       ans.add(getInorder(root, k, cnt));
+       ans.add(getInorder(root, cnt[0] - k + 1, cnt));
+    return ans;
+    }
+}
