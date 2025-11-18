8488efb8b54f1b3e3513598bc5faa7bba06f1c66
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-11-18 09:24:27 +0530

    Update and rename lowest-common-ancestor-of-a-binary-search-tree.java to lowest-common-ancestor-of-a-binary-search-tree-optimal.java

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree-optimal.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree-optimal.java
@@ -1,3 +1,5 @@
+// Optimal
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -11,17 +13,25 @@
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         TreeNode curr = root;
+        // While curr is not null
         while (curr != null) {
+            // If p and q both lie on left subtree, move left
             if (p.val < curr.val && q.val < curr.val) {
                 curr = curr.left;
             }
+            // If p and q both lie on right subtree, move right
             else if (p.val > curr.val && q.val > curr.val) {
                 curr = curr.right;
             }
+            // If p and q lie on different subtrees or if the curr node itself is one of p or q 
             else {
+                // Return curr node
                 return curr;
             }
         }
         return null;
     }
 }
+
+// T.C => O(h) ----- {h = log (n) for a balanced BST, h = n for a skewed BST}
+// S.C => O(1)
