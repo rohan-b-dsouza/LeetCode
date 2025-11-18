452ac91770a9e43db00c1867c1c407a5ad281fb4
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-11-18 14:40:27 +0530

    Update and rename construct-binary-search-tree-from-preorder-traversal.java to construct-binary-search-tree-from-preorder-traversal-optimal.java

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal-optimal.java
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal-optimal.java
@@ -1,3 +1,5 @@
+// Optimal
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -15,14 +17,23 @@
  */
 class Solution {
     public TreeNode getBst(int[] preorder, int upperBound, int[] index) {
+        // If index is out of bounds or value at current index is greater than upperBound, return null
         if (index[0] >= preorder.length || preorder[index[0]] > upperBound) return null;
+        // Create a node with the current value in the preorder array
         TreeNode root = new TreeNode(preorder[index[0]++]);
+        // Recursively call the function to link the left and right subtrees
         root.left =  getBst(preorder, root.val, index);
         root.right = getBst(preorder, upperBound, index);
+        // Return the node to link with its parent
         return root;
     }
     public TreeNode bstFromPreorder(int[] preorder) {
+        // Stores index of the preorder array
         int[] index = new int[1];
+        // Call the recursive function
         return getBst(preorder, Integer.MAX_VALUE, index);
     }
 }
+
+// T.C => O(n)
+// S.C => O(h) ----- {h = log(n) for balanced BST and h = n for skewed BST}
