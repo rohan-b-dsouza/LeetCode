c82b43e4467a7f169e672109a4277fc36cb965cc
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-11-20 20:39:30 +0530

    Update and rename two-sum-iv-input-is-a-bst.java to two-sum-iv-input-is-a-bst-optimal.java

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst-optimal.java
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst-optimal.java
@@ -1,3 +1,5 @@
+// Optimal
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -13,6 +15,10 @@
  *     }
  * }
  */
+ // Function that provides the next or before element (we use the reverse flag here to tell the function whether we want a greater or smaller element)
+// next means next element in ascending order
+// before means next element in descending order
+
  class BSTIterator {
   private Deque<TreeNode> stack; 
   private boolean reverse;
@@ -37,17 +43,26 @@
 
 class Solution {
     public boolean findTarget(TreeNode root, int k) {
-        // Initialize two objects of the BSTIterator class, 
+     // Initialize two objects of the BSTIterator class, 
     // false indicates ascending order and true indicates descending order
     BSTIterator l = new BSTIterator(root, false);
     BSTIterator r = new BSTIterator(root, true);
+    // Get the first element in asc and desc order
     int i = l.nextOrBefore();
     int j = r.nextOrBefore();
+    // While i and j are not equal
     while (i < j) {
+        // If i + j is equal to k, return true
         if ((i + j) == k) return true;
+        // Else if i + j is smaller than k, move i to next element in asc order (i.e increase i)
         else if ((i + j) < k) i = l.nextOrBefore();
+        // Else if i + j is greater than k, move j to next element in desc order (i.e decrease j) 
         else j = r.nextOrBefore(); 
     }
+    // Return false
     return false;
     }
 }
+
+// T.C => O(n)
+// S.C => O(2 * h) = O(h) ------- {h = log (n) for a balanced BST, h = n for a skewed BST}
