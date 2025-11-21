a03392b6f19fc8634dbe858d0af2c3463f0d1e7f
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-11-21 14:16:42 +0530

    Update and rename recover-binary-search-tree.java to recover-binary-search-tree-optimal.java

--- 99-recover-binary-search-tree/recover-binary-search-tree-optimal.java
+++ 99-recover-binary-search-tree/recover-binary-search-tree-optimal.java
@@ -1,3 +1,5 @@
+// Optimal
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -14,20 +16,31 @@
  * }
  */
 class Solution {
+  // Private Member Variables which can be accessed using functions of the same class (controlled access) (These are not global variables)
   private TreeNode first = null, middle = null, last = null, prev = null;
+  // Get the incorrect nodes using inorder traversal
   public void getIncorrectNodes(TreeNode root) {
+    // Set curr to root
     TreeNode curr = root;
+    // While curr is not null
     while (curr != null) {
+      // If curr's left is null
       if (curr.left == null) {
+        // If prev is null and curr's val is smaller than prev, voilation occurs
         if (prev != null && curr.val < prev.val) {
+          // If first is null, then store prev as first and curr as middle (This is done to handle the case where tree has adjacent incorrect nodes)
           if (first == null) {
             first = prev;
             middle = curr;
-          } else {
+          } 
+          // Else set last as curr
+          else {
             last = curr;
           }
         }
+        // Move prev to curr node
         prev = curr;
+        // Move curr to right
         curr = curr.right;
       } else {
         TreeNode rightMostNode = curr.left;
@@ -39,6 +52,7 @@ class Solution {
           curr = curr.left;
         } else {
           rightMostNode.right = null;
+          // Same as done before
           if (prev != null && curr.val < prev.val) {
             if (first == null) {
               first = prev;
@@ -55,18 +69,27 @@ class Solution {
   }
 
   public TreeNode recoverTree(TreeNode root) {
+    // Get the two incorrect nodes
     getIncorrectNodes(root);
+    // If first is null, return root i.e there are no incorrect nodes
     if (first == null) return root;
+    // Case 1 : Incorrect nodes are not adjacent
     if (last != null) {
+        // Swap the incorrect nodes
         int temp = first.val;
         first.val = last.val;
         last.val = temp;
     }
+    // Case 2 : Incorrect nodes are adjacent
     else {
         int temp = first.val;
         first.val = middle.val;
         middle.val = temp;
     }
+    // Return root
     return root;
   }
 }
+
+// T.C => O(n)
+// S.C => O(1)
