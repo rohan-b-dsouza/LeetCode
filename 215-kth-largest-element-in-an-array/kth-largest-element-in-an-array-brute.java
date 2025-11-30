685f63a732860ff62449d366f817d2453cd59f8a
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-11-30 19:50:18 +0530

    Update kth-largest-element-in-an-array-brute.java

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array-brute.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array-brute.java
@@ -1,6 +1,12 @@
+// Brute 
+
 public class Solution {
     public int findKthLargest(int[] nums, int k) {
+        // Sort the array and return the kth largest element
         Arrays.sort(nums);
         return nums[nums.length - k];
     }
 }
+
+// T.C => O(n * log (n))
+// S.C => O(1)
