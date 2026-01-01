468fdac56dd2fa446e4b24c5dfe2b45faf2663d2
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2026-01-01 17:50:14 +0530

    Update plus-one-optimal.java

--- 66-plus-one/plus-one-optimal.java
+++ 66-plus-one/plus-one-optimal.java
@@ -4,14 +4,18 @@ class Solution {
     public int[] plusOne(int[] digits) {
         int n = digits.length;
         for (int i = n - 1; i >= 0; i--) {
+        // If digit is smaller than 9 add 1 to it and return the array
             if (digits[i] < 9) {
                 digits[i]++;
                 return digits;
             }
+            // If digit is 9, make it zero
             digits[i] = 0;
         }
+        // If all digits are 9, create a new array with first element as 1 and all zeroes
         int[] ans = new int[n + 1];
         ans[0] = 1;
+        // Return ans
         return ans;
     }
 }
