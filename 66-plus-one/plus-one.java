0b4ff25cbbfeba3c691b10de61f1f7e45793bfa3
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2026-01-01 17:38:11 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (37.47%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -1,25 +1,15 @@
 class Solution {
     public int[] plusOne(int[] digits) {
         int n = digits.length;
-        List<Integer> list = new LinkedList<>(); 
-        int carry = 1;
         for (int i = n - 1; i >= 0; i--) {
-            int currDigit = digits[i] + carry;
-            if (currDigit == 10) {
-                carry = 1;
-                list.addFirst(0);
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
             }
-            else {
-                carry = 0;
-                list.addFirst(currDigit);
-            }
-            
+            digits[i] = 0;
         }
-        if (carry == 1) list.addFirst(carry);
-        int[] ans = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            ans[i] = list.get(i);
-        }
+        int[] ans = new int[n + 1];
+        ans[0] = 1;
         return ans;
     }
 }
