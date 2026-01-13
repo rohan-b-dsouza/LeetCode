69e8c492fe0ad655b34ed42237cc397730c4472a
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2026-01-13 19:01:37 +0530

    Time: 5 ms (57.66%) | Memory: 44.4 MB (71.32%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        int[] str = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            str[s.charAt(i) - 'a']++;
+            str[t.charAt(i) - 'a']--;
+        } 
+        for (int num : str) {
+            if (num != 0) return false;
+        }
+        return true;
+    }
+}
