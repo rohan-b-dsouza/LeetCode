dafa94bf9dd899d4f978b1d3d2a1b3726e9b15aa
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2026-01-13 19:11:59 +0530

    Update valid-anagram-optimal.java

--- 242-valid-anagram/valid-anagram-optimal.java
+++ 242-valid-anagram/valid-anagram-optimal.java
@@ -1,5 +1,8 @@
 // Optimal
 
+// Logic:- Create a freq array. Increment count of a character in case of s, and decrement in case of t. If s and t are anagrams, the no. of characters and type of characters will be balanced hence the array should have only zeroes at the end.
+// If not then return false.
+
 class Solution {
     public boolean isAnagram(String s, String t) {
         if (s.length() != t.length()) return false;
