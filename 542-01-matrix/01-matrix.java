77ab701ebedfe96f3c608231d4f4e34bdcc2a152
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2026-01-16 11:45:02 +0530

    Time: 15 ms (47.04%) | Memory: 48.8 MB (85.23%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -20,7 +20,6 @@ class Solution {
     for (int i = 0; i < n; i++) {
       for (int j = 0; j < m; j++) {
         if (mat[i][j] == 0) {
-          visited[i][j] = 0;
           queue.offer(new Tuple(i, j, 0));
         }
       }
