8dd6c1b6deb86853b0d692cd0f53839931120b55
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2026-01-19 19:59:08 +0530

    Update is-graph-bipartite-dfs.java

--- 801-is-graph-bipartite/is-graph-bipartite-dfs.java
+++ 801-is-graph-bipartite/is-graph-bipartite-dfs.java
@@ -5,10 +5,10 @@
 
 class Solution {
   public boolean dfs(int sourceNode, int[][] graph, int[] visited, int color) {
-    visited[sourceNode] = color == 1 ? 2 : 1;
+    visited[sourceNode] = color;
     for (int node : graph[sourceNode]) {
         if (visited[node] == 0) {
-           boolean flag = dfs(node, graph, visited, visited[sourceNode]);
+           boolean flag = dfs(node, graph, visited, visited[sourceNode] == 1 ? 2 : 1);
            if (!flag) return false;
         }
         else if (visited[node] == visited[sourceNode]) {
@@ -23,7 +23,7 @@ class Solution {
     int[] visited = new int[V];
     for (int i = 0; i < V; i++) {
         if (visited[i] == 0) {
-            if (!dfs(i, graph, visited, 2)) {
+            if (!dfs(i, graph, visited, 1)) {
                 return false;
             }
         }
