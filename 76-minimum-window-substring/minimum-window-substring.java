02602422d8e4276f03c5ee996ec882cf7680541a
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2026-01-21 11:56:35 +0530

    Time: 8 ms (67.64%) | Memory: 45.8 MB (74.80%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -0,0 +1,38 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        int m = s.length();
+        int n = t.length();
+        int[] hash = new int[256];
+        for (int i = 0; i < n; i++) {
+          hash[t.charAt(i)]++;
+        }
+        int start = -1;
+        int end = -1;
+        int minlen = Integer.MAX_VALUE;
+        int l = 0;
+        int cnt = 0;
+        for (int r = 0; r < m; r++) {
+          char ch = s.charAt(r);
+          if (cnt < n) {
+              if (hash[ch] > 0) {
+                cnt++;
+              }
+              hash[ch]--;
+          }
+          while(cnt == n) {
+            if (r - l + 1 < minlen) {
+              minlen = r - l + 1;
+              start = l;
+              end = r;
+            } 
+            if (hash[s.charAt(l)] >= 0) {
+              cnt--;
+            }
+            hash[s.charAt(l)]++;
+            l++;
+          }
+        }
+        if (minlen == Integer.MAX_VALUE) return "";
+        else return s.substring(start, end + 1);
+    }
+}
