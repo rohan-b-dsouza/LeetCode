2b290f3487261ab2a69117ce4fa00b381f4bac2a
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2026-01-24 20:52:17 +0530

    Update sum-of-subarray-minimums.java

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
@@ -1,29 +1,35 @@
+// Optimal 
+
+// Logic:- Logic used here is similar to that in largest histogram, just made changes to fit for this problem. Instead of precomputing nse and pse, we compute it on the fly.
+
 class Solution {
     public int sumSubarrayMins(int[] arr) {
-        int MOD = 1_000_000_007;
         int n = arr.length;
-
-        Deque<Integer> stack = new ArrayDeque<>();
         long sum = 0;
-
-        for (int i = 0; i <= n; i++) {
-            int curr = (i == n) ? Integer.MIN_VALUE : arr[i];
-
-            while (!stack.isEmpty() && arr[stack.peek()] > curr) {
-                int mid = stack.pop();
-                int left = stack.isEmpty() ? -1 : stack.peek();
-                int right = i;
-
-                long leftCount = mid - left;
-                long rightCount = right - mid;
-
-                long contribution = (arr[mid] * leftCount % MOD) * rightCount % MOD;
-                sum = (sum + contribution) % MOD;
-            }
-
-            stack.push(i);
+        int MOD = 1000000007;
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+          while(!stack.isEmpty() && arr[stack.peek()] > arr[i]) {
+            int idx = stack.pop();
+            int nse = i;
+            int pse = stack.isEmpty() ? -1 : stack.peek();
+            int left = idx - pse;
+            int right = nse - idx;
+            sum = (sum + (left * 1L * right * arr[idx]) % MOD) % MOD;
+          }
+          stack.push(i);
         }
-
+        while(!stack.isEmpty()) {
+          int idx = stack.pop();
+          int nse = n;
+          int pse = stack.isEmpty() ? -1 : stack.peek();
+          int left = idx - pse;
+          int right = nse - idx;
+          sum = (sum + (left * 1L * right * arr[idx]) % MOD) % MOD;
+        }
         return (int) sum;
     }
 }
+
+// T.C => O(n)
+// S.C => O(n)
