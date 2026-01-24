c5efc5f3851c0311ab7665d1e7b3c1932439f7c2
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2026-01-24 21:11:07 +0530

    Time: 26 ms (93.59%) | Memory: 82 MB (95.13%) - LeetSync

---
+++ 1985-maximum-subarray-min-product/maximum-subarray-min-product.java
@@ -0,0 +1,40 @@
+class Solution {
+  public int maxSumMinProduct(int[] nums) {
+    int n = nums.length;
+    int MOD = 1000000007;
+    long[] prefixSum = new long[n];
+    long sum = 0;
+    long ans = 0;
+    Deque<Integer> stack = new ArrayDeque<>();
+    for (int i = 0; i < n; i++) {
+      sum += nums[i];
+      prefixSum[i] = sum;
+    }
+    for (int i = 0; i < n; i++) {
+      while (!stack.isEmpty() && nums[stack.peek()] > nums[i]) {
+        int idx = stack.pop();
+        int nse = i;
+        int pse = stack.isEmpty() ? -1 : stack.peek();
+        int left = pse + 1;
+        int right = nse - 1;
+        if (left == 0)
+          ans = Math.max(nums[idx] * 1L * prefixSum[right], ans);
+        else
+          ans = Math.max(nums[idx] * 1L * (prefixSum[right] - prefixSum[left - 1]), ans);
+      }
+      stack.push(i);
+    }
+    while (!stack.isEmpty()) {
+      int idx = stack.pop();
+      int nse = n;
+      int pse = stack.isEmpty() ? -1 : stack.peek();
+      int left = pse + 1;
+      int right = nse - 1;
+      if (left == 0)
+        ans = Math.max(nums[idx] * 1L * prefixSum[right], ans);
+      else
+        ans = Math.max(nums[idx] * 1L * (prefixSum[right] - prefixSum[left - 1]), ans);
+    }
+    return (int) (ans % MOD);
+  }
+}
