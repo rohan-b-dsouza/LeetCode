0fb18f416946e207c00ae7b3df46de5ca6776a23
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2026-01-27 08:27:02 +0530

    Update and rename find-eventual-safe-states.java to find-eventual-safe-states-bfs.java

--- 820-find-eventual-safe-states/find-eventual-safe-states-bfs.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states-bfs.java
@@ -1,6 +1,9 @@
 // Optimal
 
-// Logic:- 
+// Logic:- Since only nodes which are not part of a cycle are safe nodes, we can use Kahn's Algorithm to solve this problem.
+// Here, we first reverse the graph. Now, all the terminal nodes will have indegree 0.
+// Hence, we will put these indegree 0 nodes in the queue & start Kahn's Algorithm.
+// Only those nodes which are not part of a cycle will be added to the topoSort list.
 
 class Solution {
     public List<Integer> topoSort(int V, List<List<Integer>> graph) {
