ede79bcbdf94122e1a8cd18507acfe457f45f0d6
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2026-01-27 08:19:23 +0530

    Update and rename find-eventual-safe-states.java to find-eventual-safe-states-dfs.java

--- 820-find-eventual-safe-states/find-eventual-safe-states-dfs.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states-dfs.java
@@ -1,3 +1,8 @@
+// Optimal
+
+// Logic:- Recursively traverse the nodes in the graph if they are not visited. At any step if a node is reaching a visited node in the same path or a node which was a part of a cycle, then return true immediately.
+// This ensures that only the nodes which are not a part of the cycle are added to the ans, as only these nodes are safe nodes.
+
 class Solution {
     public boolean dfs(int node, int[][] graph, int[] visited, int[] safeNode) {
         visited[node] = 2;
@@ -12,7 +17,7 @@ class Solution {
         return false;
     }
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        int V = graph.length;
+       int V = graph.length;
        int[] visited = new int[V];
        int[] safeNode = new int[V];
        for (int i = 0; i < V; i++) {
@@ -29,3 +34,6 @@ class Solution {
        return ans;
     }
 }
+
+// T.C => O(V + E)
+// S.C => O(V)
