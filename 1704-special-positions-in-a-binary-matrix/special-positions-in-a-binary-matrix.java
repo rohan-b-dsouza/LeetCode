19cb4129decb52018df1551e74ff473389dbf8f2
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2026-03-04 11:49:45 +0530

    Time: 2 ms (94.84%) | Memory: 47.4 MB (26.69%) - LeetSync

--- 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.java
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.java
@@ -1,22 +1,22 @@
 class Solution {
-    public boolean isSpecial(int[][] mat, int r, int c, int n, int m) {
-        for (int i = 0; i < m; i++) {
-            if (i != c && mat[r][i] == 1) return false;
-        }
-        for (int i = 0; i < n; i++) {
-            if (i != r && mat[i][c] == 1) return false;
-        }
-        return true;
-    }
     public int numSpecial(int[][] mat) {
         int n = mat.length;
         int m = mat[0].length;
         int ans = 0;
+        int[] row = new int[n];
+        int[] col = new int[m];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (mat[i][j] == 1) {
-                    boolean check = isSpecial(mat, i, j, n, m);   
-                    if (check) ans++;    
+                    row[i]++;
+                    col[j]++;
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (mat[i][j] == 1) {
+                    if (row[i] <= 1 && col[j] <= 1) ans++;
                 }
             }
         }
