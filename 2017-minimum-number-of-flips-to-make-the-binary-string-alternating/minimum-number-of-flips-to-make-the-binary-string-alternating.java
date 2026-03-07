aa0d6a27dd636bd0509ccf849f7368e633e6cfb9
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2026-03-07 16:45:02 +0530

    Time: 28 ms (36.86%) | Memory: 46.6 MB (97.88%) - LeetSync

--- 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.java
+++ 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.java
@@ -1,30 +1,28 @@
 class Solution {
     public int minFlips(String s) {
         int n = s.length();
-        StringBuilder sb = new StringBuilder(s);
-        sb.append(s);
-        StringBuilder s1 = new StringBuilder();
-        StringBuilder s2 = new StringBuilder();
-        for (int i = 0; i < 2 * n; i++) {
-            s1.append((i % 2 == 0) ? '0' : '1');
-            s2.append((i % 2 == 0) ? '1' : '0');
-        }
         int i = 0;
         int j = 0;
         int flip1 = 0;
         int flip2 = 0;
         int ans = Integer.MAX_VALUE;
         while (j < 2 * n - 1) {
-            if (sb.charAt(j) != s1.charAt(j)) {
+            char expectedCharS1 = (j % 2 == 0 ? '0' : '1');
+            char expectedCharS2 = (j % 2 == 0 ? '1' : '0');
+            if (s.charAt(j % n) != expectedCharS1) {
                 flip1++;
             }
-            if (sb.charAt(j) != s2.charAt(j)) {
+            if (s.charAt(j % n) != expectedCharS2) {
                 flip2++;
             }
             if (j - i + 1 >= n) {
+                expectedCharS1 = (i % 2 == 0 ? '0' : '1');
+                expectedCharS2 = (i % 2 == 0 ? '1' : '0');
                 ans = Math.min(flip1, Math.min(flip2, ans));
-                if (sb.charAt(i) != s1.charAt(i)) flip1 -= 1;
-                if (sb.charAt(i) != s2.charAt(i)) flip2 -= 1;
+                if (s.charAt(i) != expectedCharS1)
+                    flip1 -= 1;
+                if (s.charAt(i) != expectedCharS2)
+                    flip2 -= 1;
                 i++;
             }
             j++;
