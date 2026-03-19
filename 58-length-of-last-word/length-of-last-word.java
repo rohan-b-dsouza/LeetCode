a319f44fb056627f173bc0c3c1eba897d2bfa31d
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2026-03-19 15:54:57 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (13.29%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int n = s.length();
+        int cnt = 0;
+        int i = n - 1;
+        while (i >= 0 && Character.isWhitespace(s.charAt(i))) i--;
+        while (i >= 0 && Character.isAlphabetic(s.charAt(i))) {
+            cnt++;
+            i--;
+        };
+        return cnt;
+    }
+}
