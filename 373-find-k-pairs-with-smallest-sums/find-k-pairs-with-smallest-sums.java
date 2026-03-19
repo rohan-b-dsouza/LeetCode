49daf40e14025511c57153af24e184b7c687920b
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2026-03-19 15:39:53 +0530

    Time: 31 ms (61.49%) | Memory: 121.3 MB (54.41%) - LeetSync

--- 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.java
+++ 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.java
@@ -1,32 +1,24 @@
 class Solution {
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
         List<List<Integer>> ans = new ArrayList<>();
-        if (nums1.length == 0 || nums2.length == 0) return ans;
-
         PriorityQueue<int[]> pq = new PriorityQueue<>(
-    (a, b) -> Integer.compare(
-        nums1[a[0]] + nums2[a[1]],
-        nums1[b[0]] + nums2[b[1]]
-    )
-);
-
-        // initial push
+            (a, b)-> {
+                int compare = Integer.compare(nums1[a[0]] + nums2[a[1]], nums1[b[0]] + nums2[b[1]]);
+                return compare;
+            }
+        );
         for (int i = 0; i < Math.min(k, nums1.length); i++) {
-            pq.add(new int[]{i, 0});
+            pq.offer(new int[] {i, 0});
         }
-
         while (k-- > 0 && !pq.isEmpty()) {
             int[] curr = pq.poll();
             int i = curr[0];
             int j = curr[1];
-
             ans.add(Arrays.asList(nums1[i], nums2[j]));
-
             if (j + 1 < nums2.length) {
-                pq.add(new int[]{i, j + 1});
+                pq.offer(new int[] {i, j + 1});
             }
         }
-
         return ans;
     }
 }
