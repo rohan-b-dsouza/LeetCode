e6604d0ca7b60e409dd7653f5279ab6d40957741
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2026-03-26 17:01:22 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (87.34%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.java
+++ 1448-maximum-69-number/maximum-69-number.java
@@ -1,28 +1,18 @@
 class Solution {
     public int maximum69Number (int num) {
-        long n = num;
-        long rev = 0;
+        int n = num;
+        int cnt = 0;
+        int idx = -1;
         while (n != 0) {
-            long dig = n % 10;
-            rev = rev * 1L * 10 + dig;
+            int dig = n % 10;
+            if (dig == 6) {
+                idx = cnt;
+            }
+            cnt++;
             n = n / 10;
         }
-        long result = 0;
-        long prod = 1;
-        boolean flag = false;
-        while (rev != 0) {
-            long dig = rev % 10;
-            if (dig == 6 && !flag) {dig = 9; flag = true;};
-            result = dig * prod + result;
-            prod *= 10;
-            rev = rev / 10;
-        }
-        long finalResult = 0;
-        while (result != 0) {
-            long dig = result % 10;
-            finalResult = finalResult * 1L * 10 + dig;
-            result = result / 10; 
-        }
-        return (int) finalResult;
+        if (idx == -1) return num;
+        return  num + (int) (3 * Math.pow(10, idx));
+         
     }
 }
