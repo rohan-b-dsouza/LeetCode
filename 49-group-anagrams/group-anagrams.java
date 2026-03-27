71578531eff2cf915a9948b4d8bacc12571f6aff
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2026-03-27 10:12:25 +0530

    Time: 17 ms (26.12%) | Memory: 50.2 MB (23.08%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,21 +1,28 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         int n = strs.length;
-        Map<String, Integer> map = new HashMap<>();
-        List<List<String>> ans = new ArrayList<>();
         int idx = 0;
+        List<List<String>> ans = new ArrayList<>();
+        Map<String, Integer> map = new HashMap<>();
         for (int i = 0; i < n; i++) {
-            char[] charArr = strs[i].toCharArray();
-            Arrays.sort(charArr);
-            String s = new String(charArr);
-            if (!map.containsKey(s)) {
+            String s = strs[i];
+            int[] freq = new int[26];
+            for (int j = 0; j < s.length(); j++) {
+                freq[s.charAt(j) - 'a']++;
+            }
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < 26; j++) {
+                sb.append(freq[j]).append(',');
+            }
+            String freqStr = sb.toString();
+            if (!map.containsKey(freqStr)) {
+                map.put(freqStr, idx);
                 List<String> temp = new ArrayList<>();
-                temp.add(strs[i]);
+                temp.add(s);
                 ans.add(temp);
-                map.put(s, idx);
                 idx++;
             }
-            else ans.get(map.get(s)).add(strs[i]);
+            else ans.get(map.get(freqStr)).add(s);
         }
         return ans;
     }
