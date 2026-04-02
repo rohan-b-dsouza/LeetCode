c4f5b6185ff4f58db9f1d4246954da7b2f721b38
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2026-04-02 19:23:04 +0530

    Time: 1 ms (99.96%) | Memory: 42.8 MB (92.53%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -1,21 +1,29 @@
 class Solution {
-    public void dfs(StringBuilder temp, int n, int k, List<String> happyStrings) {
+    public boolean dfs(StringBuilder temp, int n, int[] cnt, String[] ans) {
         if (temp.length() == n) {
-            happyStrings.add(temp.toString());
-            return;
+            cnt[0]--;
+            if (cnt[0] == 0) {
+                ans[0] = temp.toString();
+                return true;
+            };
+            return false;
         }
         for (char ch = 'a'; ch <= 'c'; ch++) {
             if (temp.length() != 0 && temp.charAt(temp.length() - 1) == ch) continue;
             temp.append(ch);
-            dfs(temp, n, k, happyStrings);
+            boolean gotKthString = dfs(temp, n, cnt, ans);
+            if (gotKthString) return true;
             temp.deleteCharAt(temp.length() - 1);
         }
+        return false;
     }
     public String getHappyString(int n, int k) {
-        List<String> happyStrings = new ArrayList<>();
+        String[] ans = new String[1];
+        ans[0] = "";
+        int[] cnt = new int[1];
+        cnt[0] = k;
         StringBuilder temp = new StringBuilder();
-        dfs(temp, n, k, happyStrings);
-        happyStrings.sort(null);
-        return happyStrings.size() < k ? "" : happyStrings.get(k - 1);
+        dfs(temp, n, cnt, ans);
+        return ans[0];
     }
 }
