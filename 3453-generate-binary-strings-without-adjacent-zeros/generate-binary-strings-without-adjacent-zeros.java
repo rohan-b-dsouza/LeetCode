c62a3c6fcd343bbc21c6451987f92c32d1ddd548
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2026-04-02 14:11:17 +0530

    Time: 2 ms (90.66%) | Memory: 47.7 MB (65.13%) - LeetSync

--- 3453-generate-binary-strings-without-adjacent-zeros/generate-binary-strings-without-adjacent-zeros.java
+++ 3453-generate-binary-strings-without-adjacent-zeros/generate-binary-strings-without-adjacent-zeros.java
@@ -4,29 +4,14 @@ class Solution {
             ans.add(temp.toString());
             return;
         }
-        if (temp.length() != 0) {
-            char ch = temp.charAt(temp.length() - 1);
-            if (ch == '0') {
-                temp.append('1');
-                dfs(n, temp, ans);
-                temp.deleteCharAt(temp.length() - 1);
-            } else {
-                temp.append('0');
-                dfs(n, temp, ans);
-                temp.deleteCharAt(temp.length() - 1);
-                temp.append('1');
-                dfs(n, temp, ans);
-                temp.deleteCharAt(temp.length() - 1);
-            }
-        } else {
+        temp.append('1');
+        dfs(n, temp, ans);
+        temp.deleteCharAt(temp.length() - 1);
+        if (temp.length() == 0 || temp.charAt(temp.length() - 1) == '1') {
             temp.append('0');
             dfs(n, temp, ans);
             temp.deleteCharAt(temp.length() - 1);
-            temp.append('1');
-            dfs(n, temp, ans);
-            temp.deleteCharAt(temp.length() - 1);
         }
-
     }
 
     public List<String> validStrings(int n) {
