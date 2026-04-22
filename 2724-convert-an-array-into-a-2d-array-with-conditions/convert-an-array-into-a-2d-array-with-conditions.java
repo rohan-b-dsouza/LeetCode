83ff5b6983412255b56ba315b0963c13cb7b7874
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2026-04-22 16:19:06 +0530

    Time: 4 ms (47.30%) | Memory: 46.9 MB (42.95%) - LeetSync

--- 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.java
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.java
@@ -1,25 +1,15 @@
 class Solution {
     public List<List<Integer>> findMatrix(int[] nums) {
-        int n = nums.length;
         List<List<Integer>> ans = new ArrayList<>();
-        int cnt = 0;
-        int i = 0;
-        while (cnt != n) {
-            List<Integer> temp  = new ArrayList<>();
-            Set<Integer> st = new HashSet<>();
-            while (i < n) {
-                if (cnt == n) break;
-                if (nums[i] != -1 && !st.contains(nums[i])) {
-                    temp.add(nums[i]);
-                    st.add(nums[i]);  
-                    nums[i] = -1;
-                    cnt++;                  
-                };
-                i++;
+        Map<Integer, Integer> map = new HashMap<>();
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            map.merge(nums[i], 1, Integer::sum);
+            if (ans.size() < map.get(nums[i])) {
+                ans.add(new ArrayList<>());
             }
-            i = 0;
-            ans.add(temp);
+            ans.get(map.get(nums[i]) - 1).add(nums[i]);
         }
         return ans;
-    }   
+    }
 }
