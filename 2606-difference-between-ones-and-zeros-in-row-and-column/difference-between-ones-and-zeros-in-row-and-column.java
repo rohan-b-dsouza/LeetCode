b7996103fe75b972e7c8439ec6abb046d842bf87
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2026-05-11 22:27:35 +0530

    Time: 10 ms (49.13%) | Memory: 179.6 MB (13.65%) - LeetSync

--- 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.java
+++ 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.java
@@ -2,22 +2,18 @@ class Solution {
     public int[][] onesMinusZeros(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-        int[][] rows = new int[n][2];
-        int[][] cols = new int[m][2];
+        int[] row = new int[n];
+        int[] col = new int[m];
         for (int i = 0; i < n; i++) {
-            for (int num : grid[i]) {
-                rows[i][num]++;
-            }
-        }
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                cols[i][grid[j][i]]++;
+            for (int j = 0; j < m; j++) {
+                row[i] += grid[i][j];
+                col[j] += grid[i][j];
             }
         }
         int[][] ans = new int[n][m];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                ans[i][j] = rows[i][1] + cols[j][1] - rows[i][0] - cols[j][0];
+                ans[i][j] = row[i] + col[j] - (n - row[i]) - (m - col[j]);
             }
         }
         return ans;
