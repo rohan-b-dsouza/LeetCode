8dc19b386b9be5da7b93d8c960ff5c66e78c2e75
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2026-05-12 22:32:21 +0530

    Time: 39 ms (21.15%) | Memory: 47.3 MB (76.44%) - LeetSync

--- 1752-arithmetic-subarrays/arithmetic-subarrays.java
+++ 1752-arithmetic-subarrays/arithmetic-subarrays.java
@@ -1,17 +1,28 @@
 class Solution {
     public List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {
-        int n = nums.length;
         int m = l.length;
         List<Boolean> ans = new ArrayList<>();
         for (int i = 0; i < m; i++) {
-            List<Integer> temp = new ArrayList<>();
+            Set<Integer> temp = new HashSet<>();
+            int min = Integer.MAX_VALUE; int max = Integer.MIN_VALUE;
+            int n = r[i] - l[i] + 1;
             for (int j = l[i]; j <= r[i]; j++) {
                 temp.add(nums[j]);
+                min = Math.min(nums[j], min);
+                max = Math.max(nums[j], max);
             }
-            temp.sort(null);
+            if ((max - min) % (n - 1) != 0) {
+                ans.add(false);
+                continue;
+            }
+            int d = (max - min) / (n - 1);
+            if (d == 0) {
+                ans.add(true);
+                continue;
+            }
             boolean flag = true;
-            for (int k = 2; k < (r[i] - l[i] + 1); k++) {
-                if ((temp.get(k) - temp.get(k - 1)) != (temp.get(1) - temp.get(0))) {
+            for (int k = min; k <= max; k += d) {
+                if (!temp.contains(k)) {
                     flag = false;
                     break;
                 }
