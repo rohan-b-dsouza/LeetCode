aafac03344550270affa5e5e47890e51f80acada
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2026-07-02 19:47:15 +0530

    Time: 4 ms (96.02%) | Memory: 42.8 MB (99.04%) - LeetSync

--- 768-partition-labels/partition-labels.java
+++ 768-partition-labels/partition-labels.java
@@ -2,43 +2,19 @@ class Solution {
     public List<Integer> partitionLabels(String s) {
         int n = s.length();
         List<Integer> ans = new ArrayList<>();
-        int[][] arr = new int[26][2];
-        for (int i = 0; i < 26; i++) {
-            arr[i][0] = n;
-            arr[i][1] = n;
-        }
+        int[] last = new int[26];
         for (int i = 0; i < n; i++) {
-            int idx = s.charAt(i) - 'a';
-            if (arr[idx][0] == n) {
-                arr[idx][0] = i;
-                arr[idx][1] = i;
-            } 
-            else {
-                arr[idx][1] = i;
-            }
+            last[s.charAt(i) - 'a'] = i;
         }
+        int start = 0;
+        int end = 0;
         int i = 0;
-        int firstIdx = -1; int lastIdx = -1;
         while (i < n) {
-            int[] range = arr[s.charAt(i) - 'a']; 
-            System.out.println(firstIdx + " " + lastIdx);
-            if (range[1] == lastIdx && lastIdx == i) {
-                ans.add(lastIdx - firstIdx + 1);
-                firstIdx = -1;
-                lastIdx = -1;
-                i++;
-                continue;
+            end = Math.max(end, last[s.charAt(i) - 'a']);
+            if (i == end) {
+                ans.add(end - start + 1);
+                start = i + 1;
             }
-            lastIdx = Math.max(lastIdx, range[1]);
-            if (firstIdx == -1) {
-                firstIdx = range[0];
-            }
-            if (lastIdx == firstIdx) {
-                ans.add(1);
-                firstIdx = -1;
-                lastIdx = -1;
-            }
-
             i++;
         }
         return ans;
