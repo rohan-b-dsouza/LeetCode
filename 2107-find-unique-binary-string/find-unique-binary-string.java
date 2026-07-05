1efb35da6b9f08af65ac5e1d86d800a04cb255d5
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2026-07-05 16:45:23 +0530

    Time: 1 ms (55.09%) | Memory: 43.1 MB (60.56%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.java
+++ 2107-find-unique-binary-string/find-unique-binary-string.java
@@ -1,30 +1,16 @@
 class Solution {
-    public boolean dfs(StringBuilder sb, int n, String[] ans, Set<String> set) {
-        if (sb.length() == n) {
-            String s = sb.toString();
-            if (!set.contains(s)) {
-                ans[0] = s;
-                return true;
-            }
-            return false;
-        }
-        sb.append("0");
-        if (dfs(sb, n, ans, set)) return true;
-        sb.deleteCharAt(sb.length() - 1);
-        sb.append("1");
-        if (dfs(sb, n, ans, set)) return true;
-        sb.deleteCharAt(sb.length() - 1);
-        return false;
-    }
     public String findDifferentBinaryString(String[] nums) {
         int n = nums.length;
-        String[] ans = new String[1];
-        StringBuilder sb = new StringBuilder();
-        Set<String> set = new HashSet<>();
+        Set<String> st = new HashSet<>();
         for (int i = 0; i < n; i++) {
-            set.add(nums[i]);
+            st.add(nums[i].toString());
         }
-        dfs(sb, n, ans, set);
-        return ans[0];
+        StringBuilder sb = new StringBuilder(nums[0]);
+        for (int i = 0; i < n; i++) {
+            sb.setCharAt(i, sb.charAt(i) == '0' ? '1' : '0');
+            String s = sb.toString();
+            if (!st.contains(s)) return s; 
+        }
+        return "";
     }
 }
