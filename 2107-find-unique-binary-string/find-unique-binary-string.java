e25d6fea9f7e146b3d79a97e18eeb29580e928f5
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2026-07-05 18:01:17 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (94.02%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.java
+++ 2107-find-unique-binary-string/find-unique-binary-string.java
@@ -1,16 +1,10 @@
 class Solution {
     public String findDifferentBinaryString(String[] nums) {
         int n = nums.length;
-        Set<String> st = new HashSet<>();
+        StringBuilder ans = new StringBuilder();
         for (int i = 0; i < n; i++) {
-            st.add(nums[i].toString());
+            ans.append(nums[i].charAt(i) == '0' ? '1' : '0');
         }
-        StringBuilder sb = new StringBuilder(nums[0]);
-        for (int i = 0; i < n; i++) {
-            sb.setCharAt(i, sb.charAt(i) == '0' ? '1' : '0');
-            String s = sb.toString();
-            if (!st.contains(s)) return s; 
-        }
-        return "";
+        return ans.toString();
     }
 }
