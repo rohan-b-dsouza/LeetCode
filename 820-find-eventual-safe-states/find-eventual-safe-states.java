57254d4afe0452cce3fb1e64e21bfd72dee4e05e
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2026-07-17 19:25:34 +0530

    Time: 33 ms (10.63%) | Memory: 63.2 MB (42.95%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,30 +1,37 @@
 class Solution {
-    public boolean dfs(int source, int[] visited, int[] pathVisited, int[][] graph, List<Integer> ans) {
-        visited[source] = 1;
-        pathVisited[source] = 1;
-        for (int node: graph[source]) {
-            if (visited[node] != 1) {
-                if (dfs(node, visited, pathVisited, graph, ans)) return true;
-            }
-            else if (pathVisited[node] == 1) {
-                return true;
-            }
-        }
-        ans.add(source);
-        pathVisited[source] = 0; 
-        return false;
-    }
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        int n = graph.length;
-        List<Integer> ans = new ArrayList<>();
-        int[] visited = new int[n];
-        int[] pathVisited = new int[n];
-        for (int i = 0; i < n; i++) {
-            if (visited[i] != 1) {
-                dfs(i, visited, pathVisited, graph, ans);
+       int n = graph.length;
+       List<Integer> ans = new ArrayList<>();
+       List<List<Integer>> revGraph = new ArrayList<>();
+       Deque<Integer> queue = new ArrayDeque<>();
+       for (int i = 0; i < n; i++) revGraph.add(new ArrayList<>());
+       for (int i = 0; i < n; i++) {
+        for (int node: graph[i]) {
+            revGraph.get(node).add(i);
+        }
+       }
+       int[] indegree = new int[n];
+       for (int i = 0; i < n; i++) {
+        for (int node: revGraph.get(i)) {
+            indegree[node]++;
+        }
+       }
+       for (int i = 0; i < n; i++) {
+        if (indegree[i] == 0) {
+            queue.offer(i);
+        }
+       }
+       while (!queue.isEmpty()) {
+        int peak = queue.poll();
+        ans.add(peak);
+        for (int node: revGraph.get(peak)) {
+            indegree[node]--;
+            if (indegree[node] == 0) {
+                queue.offer(node);
             }
         }
-        ans.sort(null);
-        return ans;
+       }
+       ans.sort(null);
+       return ans;
     }
 }
