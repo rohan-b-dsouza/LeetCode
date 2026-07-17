5d13ea3eaf7f6d036d38f69aeea36a4a82aa5366
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2026-07-17 19:09:50 +0530

    Time: 13 ms (60.52%) | Memory: 60.6 MB (76.65%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,32 +1,28 @@
 class Solution {
-    public boolean dfs(int currNode, int[][] adj, int[] visited, int[] pathVisited) {
-        visited[currNode] = 1;
-        pathVisited[currNode] = 1;
-        for (int node : adj[currNode]) {
+    public boolean dfs(int source, int[] visited, int[] pathVisited, int[][] graph, List<Integer> ans) {
+        visited[source] = 1;
+        pathVisited[source] = 1;
+        for (int node: graph[source]) {
             if (visited[node] != 1) {
-                if (dfs(node, adj, visited, pathVisited))
-                    return true;
-            } else if (pathVisited[node] == 1) {
+                if (dfs(node, visited, pathVisited, graph, ans)) return true;
+            }
+            else if (pathVisited[node] == 1) {
                 return true;
             }
         }
-        pathVisited[currNode] = 0;
+        ans.add(source);
+        pathVisited[source] = 0; 
         return false;
     }
-
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        int V = graph.length;
-        int[] visited = new int[V];
-        int[] pathVisited = new int[V];
+        int n = graph.length;
         List<Integer> ans = new ArrayList<>();
-        for (int i = 0; i < V; i++) {
+        int[] visited = new int[n];
+        int[] pathVisited = new int[n];
+        for (int i = 0; i < n; i++) {
             if (visited[i] != 1) {
-                dfs(i, graph, visited, pathVisited);
+                dfs(i, visited, pathVisited, graph, ans);
             }
-        }
-        for (int i = 0; i < V; i++) {
-            if (pathVisited[i] == 0)
-                ans.add(i);
         }
         ans.sort(null);
         return ans;
