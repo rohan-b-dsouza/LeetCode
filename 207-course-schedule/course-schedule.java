15fbda918e7238e63b8fb5b69eca1afeeed6c12b
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2026-07-19 20:49:45 +0530

    Time: 4 ms (91.69%) | Memory: 46.9 MB (78.65%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,33 +1,32 @@
 class Solution {
-  public boolean dfs(int node, int[] visited, int[] pathVisited, List<List<Integer>> adj) {
-    visited[node] = 1;
-    pathVisited[node] = 1;
-    for (int curr : adj.get(node)) {
-        if (visited[curr] != 1) {
-            if (dfs(curr, visited, pathVisited, adj)) return true;
+    public boolean dfs(int source, List<List<Integer>> adj, int[] visited, int[] pathVisited) {
+        visited[source] = 1;
+        pathVisited[source] = 1;
+        for (int node: adj.get(source))  {
+            if (visited[node] != 1) {
+                if (dfs(node, adj, visited, pathVisited)) return true;
+            }
+            else if (pathVisited[node] == 1) {
+                return true;
+            }
         }
-        else if (pathVisited[curr] == 1) {
-            return true;
+        pathVisited[source] = 0;
+        return false;
+    }
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int n = numCourses;
+        int[] visited = new int[n];
+        int[] pathVisited = new int[n];
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) adj.add(new ArrayList<>());
+        for (int[] edge: prerequisites) {
+            adj.get(edge[1]).add(edge[0]);
         }
-    }
-    pathVisited[node] = 0;
-    return false;
-  }
-  public boolean canFinish(int N, int[][] arr) {
-    List<List<Integer>> adj = new ArrayList<>();
-    for (int i = 0; i < N; i++) {
-      adj.add(new ArrayList<>());
-    }
-    for (int i = 0; i < arr.length; i++) {
-      adj.get(arr[i][1]).add(arr[i][0]);
-    }
-    int[] visited = new int[N];
-    int[] pathVisited = new int[N];
-    for (int i = 0; i < N; i++) {
-        if (visited[i] != 1) {
-            if (dfs(i, visited, pathVisited, adj)) return false;
+        for (int i = 0; i < n; i++) {
+            if (visited[i] != 1) {
+                if (dfs(i, adj, visited, pathVisited)) return false;
+            }
         }
+        return true;
     }
-    return true;
-  }
 }
